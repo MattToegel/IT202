a37609965c1bf528197f0d7c64faa77d203a7d5a
Matthew a Toegel <mt85@afsconnect1.njit.edu>
2019-09-16 16:41:22 -0400

    working on class sections

---
+++ initDB.php
@@ -0,0 +1,28 @@
+<?php
+#turn error reporting on
+ini_set('display_errors',1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+
+require('config.php');
+echo $host;
+//STOP HERE ----------------------------------------------
+
+
+
+
+
+
+$connection_string = "mysql:host=$host;dbname=$database;username=$username;
+password=$password;charset=utf8mb4";
+
+try{
+	$db = new PDO($connection_string, $username, $password);
+	echo "Should have connected";
+}
+catch(Exception $e){
+	echo $e->getMessage();
+	exit("It didn't work");
+}
+?>
