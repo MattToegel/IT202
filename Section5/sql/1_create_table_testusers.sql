f37b4c964d2c12ade5afb6b1092cd76f25a6fb21
MattToegel <matthew.toegel@njit.edu>
2019-09-23 12:46:51 -0400

    sql folder and sample

---
+++ Section5/sql/1_create_table_testusers.sql
@@ -0,0 +1,6 @@
+create table if not exists `TestUsers`(
+		`id` int auto_increment not null,
+		`username` varchar(30) not null unique,
+		`pin` int default 0,
+		PRIMARY KEY (`id`)
+		) CHARACTER SET utf8 COLLATE utf8_general_ci;
