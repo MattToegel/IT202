572ea0b326bbf7d90b7bfa1bf2567336e31ed64f
MattToegel <matthew.toegel@njit.edu>
2019-09-25 17:36:53 -0400

    replaced var_dump with var_export

--- Section6/handleRequestData.php
+++ Section6/handleRequestData.php
@@ -1,5 +1,5 @@
 <?php
-	echo "<pre>" . var_dump($_GET, true) . "</pre>";
+	echo "<pre>" . var_export($_GET, true) . "</pre>";
 
 	if(isset($_GET['name'])){
 		echo "<br>Hello, " . $_GET['name'] . "<br>";
