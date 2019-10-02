e094565098dd8b9c29c50c9d610e3baea0a47a32
MattToegel <matthew.toegel@njit.edu>
2019-10-02 11:07:31 -0400

    Added samples and solution template for JS lesson

---
+++ Section7/jsSample.alert.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+	<script>
+	//Alert sample showing it blocks page loaded until user clicks
+	alert("Hello world";
+	</script>
+</head>
+<body>
+	<p id="myPara">Just showing that we loaded something...</p>
+</body>
+</html>
