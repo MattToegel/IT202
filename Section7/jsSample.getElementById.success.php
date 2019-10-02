119cda8de9a351a98572a3231cec8a72dce0a775
MattToegel <matthew.toegel@njit.edu>
2019-10-02 11:07:31 -0400

    Added samples and solution template for JS lesson

---
+++ Section7/jsSample.getElementById.success.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+	<script>
+		function pageLoaded(){
+			//showing order of execution
+			var myPara = document.getElementbyId("myPara");
+			console.log(myPara);//what value do you get?
+			//unlike the failure version of this, you should
+			//see data about your paragraph element
+		}
+	</script>
+</head>
+<!--Added onload function that's called when the html is loaded-->
+<body onload="pageLoaded();">
+	<p id="myPara">Just showing that we loaded something...</p>
+</body>
+</html>
---
+++ Section7/jsSample.solutions.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+	<script>
+	function pageLoaded(){
+		//TODO solutions
+		//Google/Explore how to create an element and add it to the DOM
+		//create a div tag, add "added new element" as the text
+		//add it to the DOM body
+
+	}
+	</script>
+</head>
+<body onload="pageLoaded();">
+	<p id="myPara">Just showing that we loaded something...</p>
+</body>
+</html>
