e0796718d91166093dbec4f0ebdfcfbea1804061
MattToegel <matthew.toegel@njit.edu>
2019-10-03 11:08:26 -0400

    added reset button

--- Section8/myForm.full.php
+++ Section8/myForm.full.php
@@ -29,6 +29,7 @@ function getName(){
 <input type="date" name="date"/>
 <!--end new content-->
 <input type="submit" value="Try it"/>
+<input type="reset" value="Clear Form"/>
 </form>
 </body>
 </html>
