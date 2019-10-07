46a01abcfd25cb0e28e1631164b8b947f172b4bc
MattToegel <matthew.toegel@njit.edu>
2019-10-07 12:31:28 -0400

    multi validation in one check

--- Section9/registration.php
+++ Section9/registration.php
@@ -1,12 +1,8 @@
+<!DOCTYPE html>
 <?php
 ini_set('display_errors',1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-function getName(){
-	if(isset($_GET['name'])){
-		echo "<p>Hello, " . $_GET['name'] . "</p>";
-	}
-}
 function checkPasswords(){
 	if(isset($_POST['password']) && isset($_POST['confirm'])){
 		if($_POST['password'] == $_POST['confirm']){
@@ -28,16 +24,29 @@ function validate(){
 	console.log(password);
 	console.log(conf);
 	let pv = document.getElementById("validation.password");
+	let succeeded = true;
 	if(password == conf){
 		
 		pv.style.display = "none";
-		return true;
-	}
-	pv.style.display = "block";
-	pv.innerText = "Passwords don't match";
-	//alert("Passwords don't match");
-	return false;
 	
+	}
+	else{
+		pv.style.display = "block";
+		pv.innerText = "Passwords don't match";
+		
+		succeeded = false;
+	}
+	var email = form.email.value;
+	var ev = document.getElementById("validation.email");
+	if(email.indexOf('@') > -1){
+		ev.style.display = "none";
+	}
+	else{
+		ev.style.display = "block";
+		ev.innerText = "Please enter a valid email address";
+		succeeded = false;
+	}
+	return succeeded;	
 }
 </script>
 </head>
@@ -45,8 +54,10 @@ function validate(){
 <div style="margin-left: 50%; margin-right:50%;">
 <form method="POST" action="#" onsubmit="return validate();">
 <input name="name" type="text" placeholder="Enter your name"/>
-<input type="password" name="password"/>
-<input type="password" name="confirm"/>
+<input name="email" type="emai" placeholder="name@example.com"/>
+<span id="validation.email" style="display:none;"></span>
+<input type="password" name="password" placeholder="Enter password"/>
+<input type="password" name="confirm" placeholder="Re-Enter password"/>
 <span style="display:none;" id="validation.password"></span>
 <input type="submit" value="Try it"/>
 </form>
