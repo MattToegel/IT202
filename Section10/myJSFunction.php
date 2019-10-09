5dd2d27d4e0e8e389496b3c3deb5addcd6911224
MattToegel <matthew.toegel@njit.edu>
2019-10-09 12:51:39 -0400

    custom js function

---
+++ Section10/myJSFunction.php
@@ -0,0 +1,33 @@
+<html>
+<head>
+<script>
+function myValidation(inputEle, checkValue){
+	let name = inputEle.name;
+	let vid = "validation." + name;
+	let vele = document.getElementById(vid);
+	let value = inputEle.value;
+	if(value === checkValue){
+		if(vele){
+			vele.remove();
+		}
+	}
+	else{
+		if(!vele){
+			vele = document.createElement("span");
+			vele.id = vid;
+			document.body.appendChild(vele);
+		}
+		vele.innerText = name + " has an invalid value";
+	}
+	return false;
+}
+</script>
+</head>
+
+<body>
+<form onsubmit="return false;">
+<input type="text" name="test" placeholder="Trying it out" 
+	onchange="myValidation(this, 'hi');"/>
+</form>
+</body>
+</html>
