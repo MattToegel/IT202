9b8fb85084174541fe69c1160a35c0a023813bd7
MattToegel <matthew.toegel@njit.edu>
2019-11-26 10:41:32 -0500

    added create course for admin only, and added admin checking

--- fallproject/dashboard.php
+++ fallproject/dashboard.php
@@ -10,7 +10,12 @@ include_once("partials/header.php");
 include_once("helpers/functions.php");
 ?>
 
-<section>Welcome, <?php get_username();?>.</section>
+<section>Welcome, <?php get_username();?>.
+<?php if(is_admin()): ?>
+	<h4>You're an admin, Harry</h4>
+<?php endif;?>
+
+</section>
 <section>
 <header>Items Due Soon</header>
 <p>Item one</p>
