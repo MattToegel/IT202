abbc3f43f60277031abad7854bd86c5d3e52b7c7
MattToegel <Matthew.toegel@njit.edu>
2019-12-03 12:41:00 -0500

    added sample dd from table

---
+++ Section16/sample_dropdown_from_table.php
@@ -0,0 +1,24 @@
+<?php
+function get_dropdown_items(){
+	require("config.php");
+	$conn_string = "mysql:host=$host;dbname=$database;charset=utf8mb4";
+	$db = new PDO($conn_string, $username, $password);
+	
+	$query = "SELECT DISTINCT name from SomeTable";
+	$stmt = $db->prepare($query);
+	$r = $stmt->execute();
+	return $stmt->fetchAll();
+}
+?>
+
+
+<?php
+$items = get_dropdown_items();
+?>
+<select>
+<?php foreach($items as $index=>$row):?>
+	<option value="<?php echo $index;?>">
+		<?php echo $row['name'];?>
+	</option>
+<?php endforeach;?>
+</select>	
