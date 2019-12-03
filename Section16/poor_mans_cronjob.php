dd725f67b7d555e7aa550ad3a56655de524673f5
MattToegel <Matthew.toegel@njit.edu>
2019-12-03 11:17:27 -0500

    Added a sample of session-based poor man's cronjob
    For running tasks at a minimal interval triggered by script accesses.

---
+++ Section16/poor_mans_cronjob.php
@@ -0,0 +1,26 @@
+<?php
+session_start();
+function poor_mans_cronjob(){
+	//change 0 here if you want to provide an initial delay
+	//otherwise it's instant
+	$next = isset($_SESSION["nextTime"])?$_SESSION["nextTime"]:0;
+	if(time() >= $next){
+		//do something here at the interval
+		
+		//rest of it is just reset and tracking logic
+		$runs = 1;
+		if(isset($_SESSION["runs"])){
+			$runs = (int)$_SESSION["runs"];
+		}
+		echo "weeee! we ran $runs times.";
+		$delay = 30;
+		if(isset($_SESSION["delay"])){
+			$delay = (int)$_SESSION['delay'];
+		}
+		$_SESSION["nextTime"] = time() + $delay;
+		$_SESSION["runs"] += 1;
+	}
+}
+
+poor_mans_cronjob();
+?>
