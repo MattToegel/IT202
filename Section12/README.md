91454d4fd2b747cb6ebf0e0adaa04a4d792fc5f4
MattToegel <matthew.toegel@njit.edu>
2020-01-18 21:12:41 -0500

    clear

--- Section12/README.md
+++
@@ -1,1 +0,0 @@
-Sample HTML5 modified from http://jsfiddle.net/bencentra/q1s8gmqv/?utm_source=website&utm_medium=embed&utm_campaign=q1s8gmqv
--- Section14/session.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();//this should be the first call on the page that's using sessions
-
-
-$_SESSION['loggedInUser'] = array('userId'=>1, 'userName'=>'Bill');
-
-echo "Saved user: " . var_export($_SESSION['loggedInUser'], true);
-
-?>
