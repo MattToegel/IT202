d8591bcd5df74462548bc0addb372f2fd61ab57e
MattToegel <matthew.toegel@njit.edu>
2020-02-07 15:40:54 -0500

    upgrading my reg

--- projectsample/register.php
+++ projectsample/register.php
@@ -5,20 +5,54 @@
 	<body>
 		<!-- This is how you comment -->
 		<form method="POST">
-			<input type="text" name="myFirstInput"/>
-			<input type="submit" value="Click it?"/>
+			<label for="email">Email: </label>
+			<input type="email" id="email" name="email" placeholder="Enter Email"/>
+			<label for="pass">Password: </label>
+			<input type="password" id="pass" name="password" placeholder="Enter password"/>
+			<label for="conf">Confirm Password: </label>
+			<input type="password" id="conf" name="confirm"/>
+			<input type="submit" value="Register"/>
 		</form>
 	</body>
 </html>
 <?php 
+ini_set('display_errors',1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
-if(!empty($_REQUEST)){
-	echo "Request:<pre>" . var_export($_REQUEST, true) . "</pre>";
-}
-if(!empty($_GET)){
-	echo "GET:<pre>" . var_export($_GET, true) . "</pre>";
-}
-if(!empty($_POST)){
-	echo "POST:<pre>" . var_export($_POST, true) . "</pre>";
+if(	   isset($_POST['email']) 
+	&& isset($_POST['password'])
+	&& isset($_POST['confirm'])
+	){
+	$pass = $_POST['password'];
+	$conf = $_POST['confirm'];
+	if($pass == $conf){
+		echo "All good, 'registering user'";
+	}
+	else{
+		echo "What's wrong with you? Learn to type";
+		exit();
+	}
+	//let's hash it
+	$pass = password_hash($pass, PASSWORD_BCRYPT);
+	echo "<br>$pass<br>";
+	//it's hashed
+	require("config.php");
+	$connection_string = "mysql:host=$dbhost;dbname=$dbdatabase;charset=utf8mb4";
+	try {
+		$db = new PDO($connection_string, $dbuser, $dbpass);
+		$stmt = $db->prepare("INSERT INTO `Users3`
+                        (email, password) VALUES
+                        (:email, :password)");
+		$email = $_POST['email'];
+        $params = array(":email"=> $email, 
+		":password"=> $pass);
+        $stmt->execute($params);
+        echo "<pre>" . var_export($stmt->errorInfo(), true) . "</pre>";
+	}
+	catch(Exception $e){
+		echo $e->getMessage();
+		exit();
+	}
 }
 ?>
