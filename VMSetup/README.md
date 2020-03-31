774bac1181cab5ce22bf1fc9011c927895c855d2
MattToegel <matthew.toegel@njit.edu>
2020-03-31 18:05:03 -0400

    moved lamp stuff into vm directory and added readme to explain usage

---
+++ VMSetup/README.md
@@ -0,0 +1,43 @@
+#Setup your own Virtual Machine#
+You may use this script to setup your own virtual machine.
+I have compiled some online resources to get a basic lamp stack configured in a single script.
+For those of you that may not know what LAMP stands for it's Linux, Apache, MySQL, and PHP.
+
+##Setup##
+1. You must run the script as sudo
+```bash sudo ./lamp.sh```
+2. You'll be prompted for some info along the way
+	1. Set a root DB password (don't forget it as it's not saved and you won't be using it to login)
+		1. Make sure this is a secure password and do not leave blank
+	2. It may ask you if you want to use apache2 or lighttpd for phpmyadmin
+		1. Choose apache2
+	3. It'll ask if you want to configure phpmyadmin with dbconfig-common
+		1. Choose Yes (easiest solution)
+	4. It'll ask for a password for phpmyadmin DB user
+		1. Leave it blank and just continue (this will generate a random password)
+	5. It'll try to detect the connected user to setup the rest of the settings for that user, by default it should correctly detect whoever is ssh'ed into the server.
+		1. Type 'y' if the user is correct, otherwise type in the correct username
+	6. Finally, it'll echo out the user's DB password. Save this as it won't show again and you'd need the root user/password to reset this.
+3. Once the setup is done you can access your site (assuming no firewall blocking) at the following locations:
+	1. apache: http://public_ip
+	2. phpmyadmin: http://public_ip/phpmyadmin
+		1. You can login with the username/password created from step 2.6
+	3. Your mysql details will be
+		1. host = localhost
+		2. db = user from step 2.5
+		3. user = user from step 2.5
+		4. password = password from step 2.6
+4. Your user should already have access to the /var/www/html folder and subdirectories/files.
+5. Git is already installed for your convience
+
+##Troubleshooting##
+* I forgot my root password
+	* ```bash You'll probably have to redo the install```
+	* ```bash sudo apt-get remove mysql-* phpmyadmin```
+	* ```bash sudo apt-get purge mysql-* phpmyadmin```
+	* ```bash sudo ./lamp.sh```
+* I forgot my user's password
+	* If you have root DB access (i.e., you remember the root password) you can login and update your user's password 
+		* ```mysql ALTER USER 'user-name'@'localhost' IDENTIFIED BY 'NEW_USER_PASSWORD';
+FLUSH PRIVILEGES;```
+	* see the first issue if you don't have the password
---
+++ VMSetup/lamp.sh
@@ -0,0 +1,119 @@
+#!/bin/bash  
+   
+ #######################################  
+ # Bash script to install an LAMP stack in ubuntu  
+ # Author: Subhash (serverkaka.com)  
+ # Source: https://www.serverkaka.com/2018/12/install-lamp-stack-in-ubuntu-using-shell-script.html
+ # Modified By: Matt Toegel 
+  
+ # Check if running as root  
+ if [ "$(id -u)" != "0" ]; then  
+   echo "This script must be run as root" 1>&2  
+   exit 1  
+ fi  
+   
+ # Ask value for mysql root password   
+ read -s -p 'db_root_password [secretpasswd]: ' db_root_password  
+ echo  
+   
+ # Update system  
+ sudo apt-get update -y  
+   
+ ## Install APache  
+ sudo apt-get install apache2 apache2-doc apache2-mpm-prefork apache2-utils libexpat1 ssl-cert -y  
+   
+ ## Install PHP  
+ apt-get install php libapache2-mod-php php-mysql -y  
+   
+ # Install MySQL database server  
+ export DEBIAN_FRONTEND="noninteractive"  
+ debconf-set-selections <<< "mysql-server mysql-server/root_password password $db_root_password"  
+ debconf-set-selections <<< "mysql-server mysql-server/root_password_again password $db_root_password"  
+ apt-get install mysql-server -y  
+   
+ # Enabling Mod Rewrite  
+ sudo a2enmod rewrite  
+ sudo php5enmod mcrypt  
+   
+ ## Install PhpMyAdmin  
+ sudo apt-get install phpmyadmin -y  
+   
+ ## Configure PhpMyAdmin  
+ echo 'Include /etc/phpmyadmin/apache.conf' >> /etc/apache2/apache2.conf  
+   
+ # Set Permissions  
+ sudo chown -R www-data:www-data /var/www  
+   
+ # Restart Apache  
+ sudo service apache2 restart  
+
+ #Matt's changes
+ function generatePassword()
+ {
+    echo "$(openssl rand -base64 12)"
+ }
+ # Install git
+ echo "Installing git"
+ apt-get install git -y
+ echo "Done installing git"
+ # Find or Get user to assign to www-data group 
+ echo "Fetching non-root user"
+ user=$(w -shf)
+ IFS=' '
+ read -a details <<< "$user"
+ sshuser=${details[0]}
+ echo "Found user: $sshuser"
+ read -p "Is this user correct? [Y / desired username]: " answer
+ if [[ "$answer" =~ ^([yY][eE][sS]|[yY])$ ]]
+ then
+    echo "Using fetched user"
+ else
+    sshuser=$answer
+    echo "Using given user $sshuser"
+ fi
+ sudo find /var/www/html -type d -exec chmod g+s {} +
+ #sudo usermod -a -G www-data $sshuser
+ #echo "Added $sshuser to www-data group"
+ #echo $(groups $sshuser)
+ #update apache permissions and give user access
+ echo "Updating html directory permissions for $sshuser"
+ #from https://askubuntu.com/a/767534
+ #reset apache permissions
+ sudo chgrp -R www-data /var/www/html
+ sudo find /var/www/html -type d -exec chmod g+rx {} +
+ sudo find /var/www/html -type f -exec chmod g+r {} +
+ #set user as owner and give access
+ sudo chown -R $sshuser /var/www/html/
+ sudo find /var/www/html -type d -exec chmod u+rwx {} +
+ sudo find /var/www/html -type f -exec chmod u+rw {} +
+ #set group bit so new files get the proper group applied
+ sudo find /var/www/html -type d -exec chmod g+s {} +
+ #setup MySQL DB and User (non-root)
+ #function modified from https://stackoverflow.com/a/44343801
+ function createMysqlDbUser()
+ {
+    SQL1="CREATE DATABASE IF NOT EXISTS ${DB_NAME};"
+    SQL2="CREATE USER '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASS}';"
+    SQL3="GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_USER}'@'%';"
+    SQL4="FLUSH PRIVILEGES;"
+
+    if [ -f /root/.my.cnf ]; then
+        $BIN_MYSQL -e "${SQL1}${SQL2}${SQL3}${SQL4}"
+    else
+        # If /root/.my.cnf doesn't exist then it'll ask for root password
+        #_arrow "Please enter root user MySQL password!"
+        #read rootPassword
+        $BIN_MYSQL -h $DB_HOST -u root -p$db_root_password -e "${SQL1}${SQL2}${SQL3}${SQL4}"
+    fi
+ }
+ #setup mysql vars
+ BIN_MYSQL=$(which mysql)
+
+ DB_HOST='localhost'
+ DB_NAME=$sshuser
+ DB_USER=$sshuser
+ DB_PASS=$(generatePassword)
+ echo "Creating DB and user for $sshuser"
+ createMysqlDbUser
+ echo "New user password is $DB_PASS";
+ 
