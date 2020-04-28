9b87c60ea60ac938b5c4aa835cb82af0d6d5d6af
MattToegel <matthew.toegel@njit.edu>
2020-04-28 15:51:07 -0400

    updated home page

--- Ler/templates/home.php
+++ Ler/templates/home.php
@@ -5,4 +5,15 @@ if (!isset($container)) {
 }
 
 ?>
-<a href="storyform.php">Create Story</a>
+<div class="jumbotron jumbotron-fluid">
+    <div class="container">
+        <h1 class="display-4">Welcome to Ler!</h1>
+        <h5>Where you can come <code>to read</code> interactive stories!</h5>
+        <p class="lead">
+            This project lets writers create interactive stories. <br>
+            The stories will contain Arcs (or sections of plot), then each Arc will give the reader 1-4 decisions.<br>
+            Based on the decision selected it'll go to a different Arc (or line of plot).<br>
+            The reader can continue until they reach 1 of the many endings.<br>
+        </p>
+    </div>
+</div>
