a2f0a7a35fc1fc444b9ca84844fab1ffe665268a
MattToegel <matthew.toegel@njit.edu>
2020-05-22 20:55:49 -0400

    added errorInfo

--- public_html/testdb.php
+++ public_html/testdb.php
@@ -12,6 +12,7 @@ try{
 				PRIMARY KEY (`id`)
 				) CHARACTER SET utf8 COLLATE utf8_general_ci");
 	$r = $stmt->execute();
+	echo var_export($stmt->errorInfo(), true);
 	echo var_export($r, true);
 }
 catch (Exception $e){
