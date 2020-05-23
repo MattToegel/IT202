a4e8a7c82ab7fcc45f3c836219ca5469d0cac04b
MattToegel <matthew.toegel@njit.edu>
2020-05-22 22:34:38 -0400

    migrated to jawsdb and updated test sample

--- public_html/testdb.php
+++ public_html/testdb.php
@@ -8,6 +8,7 @@ try{
 				`id` int auto_increment not null,
 				`email` varchar(100) not null unique,
 				`created` timestamp default current_timestamp,
+				`modified` timestamp default current_timestamp on update current_timestamp,
 				PRIMARY KEY (`id`)
 				) CHARACTER SET utf8 COLLATE utf8_general_ci");
 	$r = $stmt->execute();
