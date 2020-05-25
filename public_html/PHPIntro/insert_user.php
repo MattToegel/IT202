e10816077a22d29099e5cd4b1e3f174ac16e274e
MattToegel <matthew.toegel@njit.edu>
2020-05-24 21:32:26 -0400

    insert_user.php sample

---
+++ public_html/PHPIntro/insert_user.php
@@ -0,0 +1,17 @@
+<?php
+require("config.php");
+
+$connection_string = "mysql:host=$dbhost;dbname=$dbdatabase;charset=utf8mb4";
+try{
+	$db = new PDO($connection_string, $dbuser, $dbpass);
+	$stmt = $db->prepare("INSERT INTO User (email) VALUES(:email)");
+	$r = $stmt->execute(array(
+		":email"=>"test@test.com"
+	));
+	echo var_export($r, true);
+	echo var_export($stmt->errorInfo(), true);
+}
+catch (Exception $e){
+	echo $e->getMessage();
+}
+?>
