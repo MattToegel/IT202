51444323d81b11e412fb8b20d5571cbfe6f47c59
MattToegel <matthew.toegel@njit.edu>
2020-05-24 21:42:53 -0400

    fixed typo in table name

--- public_html/PHPIntro/insert_user.php
+++ public_html/PHPIntro/insert_user.php
@@ -4,7 +4,7 @@ require("config.php");
 $connection_string = "mysql:host=$dbhost;dbname=$dbdatabase;charset=utf8mb4";
 try{
 	$db = new PDO($connection_string, $dbuser, $dbpass);
-	$stmt = $db->prepare("INSERT INTO User (email) VALUES (:email)");
+	$stmt = $db->prepare("INSERT INTO Users (email) VALUES (:email)");
 	$r = $stmt->execute(array(
 		":email"=>"test@test.com"
 	));
