72669113d2b104e9c2bacd135fd5fcc8f2d8ee7f
MattToegel <matthew.toegel@njit.edu>
2020-05-24 23:05:11 -0400

    update sql sample

---
+++ public_html/PHPIntro/update_user.php
@@ -0,0 +1,18 @@
+<?php
+require("config.php");
+
+$connection_string = "mysql:host=$dbhost;dbname=$dbdatabase;charset=utf8mb4";
+try{
+	$db = new PDO($connection_string, $dbuser, $dbpass);
+	$stmt = $db->prepare("UPDATE Users set email=:email WHERE email = :original");
+	$stmt->bindValue(":email","newemail@test.com");
+	$stmt->bindValue(":original","test@test.com");
+	$r = $stmt->execute();
+	
+	echo var_export($stmt->errorInfo(), true);
+	echo var_export($r, true);
+}
+catch (Exception $e){
+	echo $e->getMessage();
+}
+?>
