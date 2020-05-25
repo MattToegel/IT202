2a649b814594580fc5b3825225cb6c81fecf02ee
MattToegel <matthew.toegel@njit.edu>
2020-05-24 21:41:10 -0400

    sample insert

--- public_html/PHPIntro/insert_user.php
+++ public_html/PHPIntro/insert_user.php
@@ -1,15 +1,15 @@
 <?php
-require(__DIR__ . "/config.php");
+require("config.php");
 
 $connection_string = "mysql:host=$dbhost;dbname=$dbdatabase;charset=utf8mb4";
 try{
 	$db = new PDO($connection_string, $dbuser, $dbpass);
-	$stmt = $db->prepare("INSERT INTO User (email) VALUES(:email)");
+	$stmt = $db->prepare("INSERT INTO User (email) VALUES (:email)");
 	$r = $stmt->execute(array(
 		":email"=>"test@test.com"
 	));
-	echo var_export($r, true);
 	echo var_export($stmt->errorInfo(), true);
+	echo var_export($r, true);
 }
 catch (Exception $e){
 	echo $e->getMessage();
