cd74cfa23becf44fc97abc821a5da3a31ff9263d
MattToegel <matthew.toegel@njit.edu>
2020-05-30 16:08:21 -0400

    basic reg sample

--- public_html/SampleRegLogin/register.php
+++ public_html/SampleRegLogin/register.php
@@ -1,9 +1,9 @@
 <form method="POST">
 	<label for="email">Email
-	<input type="email" id="email" name="email"/>
+	<input type="email" id="email" name="email" autocomplete="off" />
 	</label>
 	<label for="p">Password
-	<input type="password" id="p" name="password"/>
+	<input type="password" id="p" name="password" autocomplete="off"/>
 	</label>
 	<label for="cp">Confirm Password
 	<input type="password" id="cp" name="cpassword"/>
@@ -16,11 +16,32 @@
 //echo var_export($_POST, true);
 //echo var_export($_REQUEST, true);
 if(isset($_POST["register"])){
-	if(isset($_POST["password"]) && isset($_POST["cpassword"])){
+	if(isset($_POST["password"]) && isset($_POST["cpassword"]) && isset($_POST["email"])){
 		$password = $_POST["password"];
 		$cpassword = $_POST["cpassword"];
+		$email = $_POST["email"];
 		if($password == $cpassword){
-			echo "<div>Passwords Match</div>";
+			//echo "<div>Passwords Match</div>";
+			require("config.php");
+			$connection_string = "mysql:host=$dbhost;dbname=$dbdatabase;charset=utf8mb4";
+			try{
+				$db = new PDO($connection_string, $dbuser, $dbpass);
+				$stmt = $db->prepare("INSERT INTO Users (email, password) VALUES(:email, :password)");
+				$stmt->execute(array(
+					":email" => $email,
+					":password" => $password
+				));
+				$e = $stmt->errorInfo();
+				if($e[0] != "00000"){
+					echo var_export($e, true);
+				}
+				else{
+					echo "<div>Successfully registered!</div>";
+				}
+			}
+			catch (Exception $e){
+				echo $e->getMessage();
+			}
 		}
 		else{
 			echo "<div>Passwords don't match</div>";
