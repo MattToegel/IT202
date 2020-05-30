1ec69b622d50e7d81e1a0d63236d05387d3b6f5e
MattToegel <matthew.toegel@njit.edu>
2020-05-30 15:58:48 -0400

    checking data for register.php

--- public_html/SampleRegLogin/register.php
+++ public_html/SampleRegLogin/register.php
@@ -12,7 +12,19 @@
 </form>
 
 <?php
-echo var_export($_GET, true);
-echo var_export($_POST, true);
-echo var_export($_REQUEST, true);
+//echo var_export($_GET, true);
+//echo var_export($_POST, true);
+//echo var_export($_REQUEST, true);
+if(isset($_POST["register"])){
+	if(isset($_POST["password") && isset($_POST["cpassword"])){
+		$password = $_POST["password"];
+		$cpassword = $_POST["cpassword"];
+		if($password == $cpassword){
+			echo "<div>Passwords Match</div>";
+		}
+		else{
+			echo "<div>Passwords don't match</div>";
+		}
+	}
+}
 ?>
