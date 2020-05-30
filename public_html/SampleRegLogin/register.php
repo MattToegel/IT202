c5b3d354d37ea0565de7840d8cf01dfda9a6ae99
MattToegel <matthew.toegel@njit.edu>
2020-05-30 15:53:45 -0400

    WIP of register form

--- public_html/SampleRegLogin/register.php
+++ public_html/SampleRegLogin/register.php
@@ -1,18 +1,18 @@
-<form method="POST" onsubmit="return validate_passwords();">
+<form method="POST">
 	<label for="email">Email
 	<input type="email" id="email" name="email"/>
 	</label>
 	<label for="p">Password
 	<input type="password" id="p" name="password"/>
 	</label>
-	<label for="pc">Confirm Password
-	<input type="password" id="pc" name="cpassword"/>
+	<label for="cp">Confirm Password
+	<input type="password" id="cp" name="cpassword"/>
 	</label>
 	<input type="submit" name="register" value="Register"/>
 </form>
-<script>
-function validate_passwords(){
-	console.log(this);
-	return form.password.value == form.cpassword.value;
-}
-</script>
+
+<?php
+echo var_export($_GET, true);
+echo var_export($_POST, true);
+echo var_export($_REQUEST, true);
+?>
