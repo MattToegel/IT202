092490425c62b5320962385af8538c1c278d3e63
Matt <matt@etherealswordproductions.com>
2020-06-08 10:51:20 -0400

    Temporary removal of phpmyadmin install

--- public_html/RegLoginCSS/header.php
+++ public_html/RegLoginCSS/header.php
@@ -6,3 +6,19 @@
 require("config.php");
 session_start();
 ?>
+<nav>
+    <ul>
+        <li>
+            <a href="home.php">Home</a>
+        </li>
+        <li>
+            <a href="login.php">Login</a>
+        </li>
+        <li>
+            <a href="register.php">Register</a>
+        </li>
+        <li>
+            <a href="logout.php">Logout</a>
+        </li>
+    </ul>
+</nav>
