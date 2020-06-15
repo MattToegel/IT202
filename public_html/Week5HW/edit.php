75afac746e4a2a2ace4ab35717c596d725c31155
MattToegel <matthew.toegel@njit.edu>
2020-06-14 21:01:05 -0400

    updated hw template

--- public_html/Week5HW/edit.php
+++ public_html/Week5HW/edit.php
@@ -4,4 +4,6 @@ $db = getDB();
 //example usage, change/move as needed
 $stmt = $db->prepare("SELECT * FROM Things");
 $stmt->execute();
+$result = $stmt->fetch(PDO::FETCH_ASSOC);
+echo var_export($result, true);
 ?>
