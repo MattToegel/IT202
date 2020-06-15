81189592cb2198e0b0ba8e27563b9bd5f202ec01
Matt <matt@etherealswordproductions.com>
2020-06-15 19:42:05 -0400

    fixed typos in tables

--- public_html/BasicTables/CREATE_TABLE_CART.sql
+++ public_html/BasicTables/CREATE_TABLE_CART.sql
@@ -1,11 +1,11 @@
-CREATE TABLE Cart{
+CREATE TABLE Cart(
     id int auto_increment,
     product_id int,
     quantity int,
     user_id int,
     created datetime default current_timestamp ,
-    modified datetime default current_timestamp onupdate current_timestamp,
+    modified datetime default current_timestamp on update current_timestamp,
     primary key(id),
     foreign key(product_id) references Products.id,
     foreign key(user_id) references Users.id
-}
+)
