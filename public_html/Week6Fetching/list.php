4051a4ca85d9c13b0e8f7d9388b00bf87f9eab72
Matt <matt@etherealswordproductions.com>
2020-06-20 18:12:57 -0400

    week 6 samples

---
+++ public_html/Week6Fetching/list.php
@@ -0,0 +1,35 @@
+<?php
+require("common.inc.php");
+$query = file_get_contents(__DIR__ . "/SELECT_ALL_TABLE_THINGS.sql");
+if(isset($query) && !empty($query)){
+    try {
+        $stmt = getDB()->prepare($query);
+        //we don't need to pass any arguments since we're not filtering the results
+        $stmt->execute();
+        //Note the fetchAll(), we need to use it over fetch() if we expect >1 record
+        $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+    catch (Exception $e){
+        echo $e->getMessage();
+    }
+}
+?>
+<!--This part will introduce us to PHP templating,
+note the structure and the ":" -->
+<!-- note how we must close each check we're doing as well-->
+<?php if(isset($results)):?>
+    <p>This shows when we have results</p>
+    <ul>
+        <!-- Here we'll loop over all our results and reuse a specific template for each iteration,
+        we're also using our helper function to safely return a value based on our key/column name.-->
+        <?php foreach($results as $row):?>
+            <li>
+                <?php echo get($row, "name")?>
+                <?php echo get($row, "quantity");?>
+                <a href="delete.php?thingId=<?php echo get($row, "id");?>">Delete</a>
+            </li>
+        <?php endforeach;?>
+    </ul>
+<?php else:?>
+    <p>This shows when we don't have results</p>
+<?php endif;?>
