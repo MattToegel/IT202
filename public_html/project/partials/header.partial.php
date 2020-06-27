9f90a517e5a5830199ffb7aa40a58167959a993d
MattToegel <matthew.toegel@njit.edu>
2020-06-27 18:14:10 -0400

    added link to game.php, added login check to links

--- public_html/project/partials/header.partial.php
+++ public_html/project/partials/header.partial.php
@@ -1,19 +1,28 @@
 <?php
 require_once (__DIR__."/../includes/common.inc.php");
+$logged_in = Common::is_logged_in(false);
 ?>
 <nav>
     <ul>
+        <?php if($logged_in):?>
         <li>
             <a href="<?php echo Common::url_for("home");?>">Home</a>
         </li>
+        <li>
+            <a href="<?php echo Common::url_for("game");?>">Game</a>
+        </li>
+        <?php endif; ?>
+        <?php if(!$logged_in):?>
         <li>
             <a href="<?php echo Common::url_for("login");?>">Login</a>
         </li>
         <li>
             <a href="<?php echo Common::url_for("register");?>">Register</a>
         </li>
+        <?php else:?>
         <li>
             <a href="<?php echo Common::url_for("logout");?>">Logout</a>
         </li>
+        <?php endif; ?>
     </ul>
 </nav>
