3ad0146ec9b9e64e0a7521e0157a761eac7d455e
MattToegel <matthew.toegel@njit.edu>
2020-06-27 17:52:33 -0400

    added register.php

---
+++ public_html/project/register.php
@@ -0,0 +1,39 @@
+<?php
+include(__DIR__."/partials/header.partial.php");
+?>
+    <div>
+        <h4>Register</h4>
+        <form method="POST">
+            <div>
+                <label for="email">Email</label>
+                <input type="email" id="email" name="email" required/>
+            </div>
+            <div>
+                <label for="password">Password</label>
+                <input type="password" id="password" name="password" required min="3"/>
+            </div>
+            <div>
+                <label for="cpassword">Confirm Password</label>
+                <input type="password" id="cpassword" name="cpassword" required min="3"/>
+            </div>
+            <input type="submit" name="submit" value="Register"/>
+        </form>
+    </div>
+<?php
+if (Common::get($_POST, "submit", false)){
+    $email = Common::get($_POST, "email", false);
+    $password = Common::get($_POST, "password", false);
+    $confirm_password = Common::get($_POST, "cpassword", false);
+    if($password != $confirm_password){
+        Common::flash("Passwords must match", "warning");
+        die(header("Location: register.php"));
+    }
+    if(!empty($email) && !empty($password)){
+        $result = DBH::register($email, $password);
+        echo var_export($result, true);
+    }
+    else{
+        Common::flash("Email and password must not be empty", "warning");
+        die(header("Location: register.php"));
+    }
+}
