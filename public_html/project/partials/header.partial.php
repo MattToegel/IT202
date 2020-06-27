6f1a63d9b4aef9ecb9f73a00b059a43839c66e2c
Matt <matt@etherealswordproductions.com>
2020-06-27 18:44:30 -0400

    added flash message output

--- public_html/project/partials/header.partial.php
+++ public_html/project/partials/header.partial.php
@@ -30,6 +30,7 @@ $logged_in = Common::is_logged_in(false);
     <?php $flash_messages = Common::getFlashMessages();?>
     <?php if(isset($flash_messages) && count($flash_messages) > 0):?>
         <?php foreach($flash_messages as $msg):?>
+        <?php echo var_export($msg);?>
         <div class="<?php echo Common::get($msg, 0);?>"><?php
             echo Common::get($msg, 1);
             //We have the opening and closing tags right after/before the div tags to remove any whitespace characters
