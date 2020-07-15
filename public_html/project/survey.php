9518d24da658b51f07569a4ae99e8163443ec4af
MattToegel <matthew.teogel@njit.edu>
2020-07-15 18:09:51 -0400

    adjustments for fetching data

--- public_html/project/survey.php
+++ public_html/project/survey.php
@@ -30,18 +30,19 @@ if(Common::get($response, "status", 400) == 200){
                 <?php foreach($s as $question):?>
                 <div class="list-group-item btn-group-toggle bg-light" data-toggle="buttons">
                     <?php
-                    $id = Common::get($question, "question_id") . "-" . Common::get($question, "answer_id");
+                    $id = "answer-".Common::get($question, "question_id") . "-" . Common::get($question, "answer_id");
                     ?>
                     <?php if(Common::get($question, "open_ended", false)):?>
                         <label for="<?php echo $id;?>"><?php echo Common::get($question, "answer");?></label>
                         <input id="<?php echo $id;?>" class="form-control" type="text"
-                               name="answer-<?php echo Common::get($question,"answer_id", -1);?>"/>
+                               name="question-<?php echo Common::get($question,"question_id", -1);?>"/>
                     <?php else:?>
 
                         <label class="btn btn-secondary btn-lg btn-block" for="<?php echo $id;?>">
                             <?php echo Common::get($question, "answer");?>
                         <input id="<?php echo $id;?>" autocomplete="off" type="radio"
-                               name="answer-<?php echo Common::get($question,"answer_id", -1);?>"/>
+                               name="question-<?php echo Common::get($question,"question_id", -1);?>"
+                                value="<?php echo Common::get($question, "answer_id", -1);?>" />
                         </label>
                     <?php endif; ?>
                 </div>
