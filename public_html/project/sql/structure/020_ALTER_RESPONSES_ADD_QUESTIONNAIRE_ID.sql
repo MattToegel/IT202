aa046d044f7238362111aa15a48e4246a72f0758
MattToegel <matthew.teogel@njit.edu>
2020-07-15 16:10:53 -0400

    fixed typo in alter statement

--- public_html/project/sql/structure/020_ALTER_RESPONSES_ADD_QUESTIONNAIRE_ID.sql
+++ public_html/project/sql/structure/020_ALTER_RESPONSES_ADD_QUESTIONNAIRE_ID.sql
@@ -1,3 +1,3 @@
 ALTER TABLE Responses
     ADD COLUMN `questionnaire_id` int,
-    ADD FOREIGN KEY `questionnaire_id` REFERENCES Questionnaires(`id`) ON DELETE CASCADE
+    ADD FOREIGN KEY (`questionnaire_id`) REFERENCES Questionnaires(`id`) ON DELETE CASCADE
