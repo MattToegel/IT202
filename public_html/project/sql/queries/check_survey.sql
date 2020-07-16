0fb98b3ddd64965e2978d83a18a60c7d919c00e2
MattToegel <matthew.teogel@njit.edu>
2020-07-16 18:27:08 -0400

    fixed check survey query

--- public_html/project/sql/queries/check_survey.sql
+++ public_html/project/sql/queries/check_survey.sql
@@ -1,8 +1,12 @@
 SELECT
-       count(IF(date(r.created) = curdate(),true, null)) as responses_today,
-       q.use_max,
-       count(r.created) as responses_total,
-       q.max_attempts,
-       q.attempts_per_day
-FROM Questionnaires as q
-    join Responses r on q.id = r.questionnaire_id where q.id = :qid and r.user_id = :uid
+    count(IF(date(r.created) = curdate(),true, null)) as responses_today,
+    q.use_max,
+    count(r.created) as responses_total,
+    q.max_attempts,
+    q.attempts_per_day
+FROM
+    (SELECT id, use_max, max_attempts, attempts_per_day FROM Questionnaires WHERE id = :qid ) as q
+        left join
+    (SELECT questionnaire_id, created FROM Responses where user_id = :uid) as r
+
+    on q.id = r.questionnaire_id
