18ffd45abab213687ba441941c12caf88bd3d9d8
MattToegel <matthew.teogel@njit.edu>
2020-07-16 16:48:01 -0400

    updated questionnaire form to apply HTML validations

--- public_html/project/create_questionnaire.php
+++ public_html/project/create_questionnaire.php
@@ -13,7 +13,7 @@ $last_updated = Common::get($_SESSION, "last_sync", false);
 <form method="POST">
     <div class="form-group">
         <label for="questionnaire_name">Questionnaire Name</label>
-        <input class="form-control" type="text" id="questionnaire_name" name="questionnaire_name"/>
+        <input class="form-control" type="text" id="questionnaire_name" name="questionnaire_name" required/>
     </div>
     <div class="form-group">
         <label for="questionnaire_desc">Questionnaire Description</label>
@@ -21,11 +21,11 @@ $last_updated = Common::get($_SESSION, "last_sync", false);
     </div>
     <div class="form-group">
         <label for="attempts_per_day">Attempts per day</label>
-        <input class="form-control" type="number" id="attempts_per_day" name="attempts_per_day" value="1"/>
+        <input class="form-control" type="number" id="attempts_per_day" name="attempts_per_day" value="1" min="1"/>
     </div>
     <div class="form-group">
         <label for="max_attempts">Max Attempts</label>
-        <input class="form-control" type="number" id="max_attempts" name="max_attempts" value="1"/>
+        <input class="form-control" type="number" id="max_attempts" name="max_attempts" value="1" min="0" />
     </div>
     <div class="form-group">
         <label for="use_max">Use Max?</label>
@@ -35,14 +35,14 @@ $last_updated = Common::get($_SESSION, "last_sync", false);
         <div class="list-group-item">
             <div class="form-group">
                 <label for="question_0">Question</label>
-                <input class="form-control" type="text" id="question_0" name="question_0"/>
+                <input class="form-control" type="text" id="question_0" name="question_0" required/>
             </div>
             <button class="btn btn-danger" onclick="event.preventDefault(); deleteMe(this);">X</button>
             <div class="list-group">
                 <div class="list-group-item">
                     <div class="form-group">
                         <label for="question_0_answer_0">Answer</label>
-                        <input class="form-control" type="text" id="question_0_answer_0" name="question_0_answer_0"/>
+                        <input class="form-control" type="text" id="question_0_answer_0" name="question_0_answer_0" required/>
                     </div>
                     <button class="btn btn-danger" onclick="event.preventDefault(); deleteMe(this);">X</button>
                     <div class="form-group">
