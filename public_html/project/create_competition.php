5952df67a4c654102ccd7ecdb2453b9ae43d2b78
MattToegel <matthew.teogel@njit.edu>
2020-07-20 20:20:39 -0400

    added create competition to header, reoranized createc_competition to fix header already sent

--- public_html/project/create_competition.php
+++ public_html/project/create_competition.php
@@ -1,13 +1,70 @@
 <?php
 include_once(__DIR__."/partials/header.partial.php");
-
 if(Common::is_logged_in()){
     //this will auto redirect if user isn't logged in
-    if(!Common::has_role("Admin")){
-        die(header("Location: home.php"));
-    }
 }
-$last_updated = Common::get($_SESSION, "last_sync", false);
+?>
+<?php
+if(Common::get($_POST, "submit", false)){
+    $points = Common::clamp(Common::get($_POST,"reward", 1), 1, 50);
+    $available = Common::get($_SESSION["user"], "points", 0);
+    $error = false;
+    if(($points+1) <= $available) {
+        $result = DBH::changePoints(Common::get_user_id(), -($points+1), -1, "create_competition", "created competition");
+        if(Common::get($result, "status", 400) == 200) {
+            $competition = [
+                "title" => Common::get($_POST, "competition_name", false),
+                "duration" => Common::clamp(Common::get($_POST, "duration", 1), 1, 365),
+                "first_place" => round(Common::clamp(Common::get($_POST, "fpp", 1.0)/100, 0.1, 1.0), 1),
+                "second_place" => round(Common::clamp(Common::get($_POST, "spp", 0.0)/100, 0.0, 1.0), 1),
+                "third_place" => round(Common::clamp(Common::get($_POST, "tpp", 1.0)/100, 0.0, 1.0), 1),
+                "entry_fee" => Common::clamp(Common::get($_POST, "entry_fee", 0), 0, 10000),
+                "increment_on_entry" => Common::get($_POST, "increment_entry", false) ? 1 : 0,
+                "percent_of_entry" => round(Common::clamp(Common::get($_POST, "eci", 0.0)/100, 0.0, 1.0), 1),
+                "min_participants" => Common::clamp(Common::get($_POST, "min_participants", 3), 3, 10000),
+                "points" => $points
+            ];
+            //calculate expires date
+            $date = new DateTime();
+            $interval = new DateInterval('' . join(['P', $competition["duration"], 'D']));
+            $date->add($interval);
+            $competition["expires"] = $date->format("Y-m-d");
+            $competition["user_id"] = Common::get_user_id();
+
+            $fp = $competition["first_place"];
+            $sp = $competition["second_place"];
+            $tp = $competition["third_place"];
+            if (round($fp + $sp + $tp, 1) <= round(1.0, 1)) {
+                //ok
+                $result = DBH::create_competition($competition);
+                if(Common::get($result, "status", 400) == 200){
+                    $available -= ($points+1);
+                    $_SESSION["user"]["points"] = $available;
+                    Common::flash("Created Competition", "success");
+                }
+                else{
+                    $error = true;
+                    Common::flash("Error creating Competition", "danger");
+                }
+            }
+            else {
+                $error = true;
+                Common::flash("First, Second, and Third place percentages must be less than or equal to 100%", "warning");
+            }
+        }
+        else{
+            $error = true;
+            Common::flash("Error deducting points. Create Competition Canceled", "warning");
+        }
+        if(!$error){
+            $result = DBH::changePoints(Common::get_user_id(), ($points+1), -1, "refund", "error creating competition");
+        }
+    }
+    else{
+        Common::flash("You can't afford to create a competition right now", "warning");
+    }
+    die(header("Location: create_competition.php"));
+}
 ?>
 <div class="container-fluid">
     <form method="POST" onsubmit="return checkPercentages();">
@@ -61,68 +118,6 @@ $last_updated = Common::get($_SESSION, "last_sync", false);
             <input type="submit" id="submit" name="submit" class="btn btn-primary" value="Create Competition (Cost: 1)"/>
         </div>
     </form>
-    <?php
-    if(Common::get($_POST, "submit", false)){
-        $points = Common::clamp(Common::get($_POST,"reward", 1), 1, 50);
-        $available = Common::get($_SESSION["user"], "points", 0);
-        $error = false;
-        if(($points+1) <= $available) {
-            $result = DBH::changePoints(Common::get_user_id(), -($points+1), -1, "create_competition", "created competition");
-            if(Common::get($result, "status", 400) == 200) {
-                $competition = [
-                    "title" => Common::get($_POST, "competition_name", false),
-                    "duration" => Common::clamp(Common::get($_POST, "duration", 1), 1, 365),
-                    "first_place" => round(Common::clamp(Common::get($_POST, "fpp", 1.0)/100, 0.1, 1.0), 1),
-                    "second_place" => round(Common::clamp(Common::get($_POST, "spp", 0.0)/100, 0.0, 1.0), 1),
-                    "third_place" => round(Common::clamp(Common::get($_POST, "tpp", 1.0)/100, 0.0, 1.0), 1),
-                    "entry_fee" => Common::clamp(Common::get($_POST, "entry_fee", 0), 0, 10000),
-                    "increment_on_entry" => Common::get($_POST, "increment_entry", false) ? 1 : 0,
-                    "percent_of_entry" => round(Common::clamp(Common::get($_POST, "eci", 0.0)/100, 0.0, 1.0), 1),
-                    "min_participants" => Common::clamp(Common::get($_POST, "min_participants", 3), 3, 10000),
-                    "points" => $points
-                ];
-                //calculate expires date
-                $date = new DateTime();
-                $interval = new DateInterval('' . join(['P', $competition["duration"], 'D']));
-                $date->add($interval);
-                $competition["expires"] = $date->format("Y-m-d");
-                $competition["user_id"] = Common::get_user_id();
-
-                $fp = $competition["first_place"];
-                $sp = $competition["second_place"];
-                $tp = $competition["third_place"];
-                if (round($fp + $sp + $tp, 1) <= round(1.0, 1)) {
-                    //ok
-                    $result = DBH::create_competition($competition);
-                    if(Common::get($result, "status", 400) == 200){
-                        $available -= ($points+1);
-                        $_SESSION["user"]["points"] = $available;
-                        Common::flash("Created Competition", "success");
-                    }
-                    else{
-                        $error = true;
-                        Common::flash("Error creating Competition", "danger");
-                    }
-                }
-                else {
-                    $error = true;
-                    Common::flash("First, Second, and Third place percentages must be less than or equal to 100%", "warning");
-                }
-            }
-            else{
-                $error = true;
-                Common::flash("Error deducting points. Create Competition Canceled", "warning");
-            }
-            if(!$error){
-                $result = DBH::changePoints(Common::get_user_id(), ($points+1), -1, "refund", "error creating competition");
-            }
-        }
-        else{
-            Common::flash("You can't afford to create a competition right now", "warning");
-        }
-        die(header("Location: create_competition.php"));
-    }
-    ?>
     <script>
         function toggleEntryIncrement(ele){
             if(ele.checked){
--- public_html/project/partials/header.partial.php
+++ public_html/project/partials/header.partial.php
@@ -27,6 +27,9 @@ $logged_in = Common::is_logged_in(false);
         </li>
         <?php endif;?>
         <li class="nav-item">
+            <a class="nav-link" href="<?php echo Common::url_for("create_competition");?>">Create Competition</a>
+        </li>
+        <li class="nav-item">
             <a class="nav-link" href="<?php echo Common::url_for("surveys");?>">Surveys</a>
         </li>
         <?php endif; ?>
