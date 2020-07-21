167f5838409bc757a0f6f820978f26e40cb233a4
MattToegel <matthew.teogel@njit.edu>
2020-07-21 19:58:27 -0400

    added some anti cheat checks

--- public_html/project/api/save_score.php
+++ public_html/project/api/save_score.php
@@ -13,43 +13,52 @@ if(isset($_POST["score"]) && isset($_POST["outcome"])) {
         $dmgValidation = true;
         $ph = null;
         $th = null;
-        foreach($data as $d){
-            $pt = json_decode($d[0], true);
-            $et = json_decode($d[1], true);
-            $phealth = (float)$pt["h"];
-            $ehealth = (float)$et["h"];
-            if(!isset($ph)){
-                $ph = $phealth;
-            }
-            if(!isset($th)){
-                $th = $ehealth;
-            }
-            if($phealth > $ph || $ehealth > $th){
-                $healthValidation = false;
-                error_log("Anti-cheat: invalid health, potential healing");
-                break;
-            }
-            $pdmg = (float)$pt["d"];
-            $edmg = (float)$et["d"];
-            if($edmg < ($pdmg * .45)){
-                error_log("Anti-cheat: Enemy tank damage nerfed less than allowable offset");
-                $dmgValidation = false;
-                break;
-            }
-            //TODO add other validations as necessary, got lazy to validate other status, but the above are the important ones
-            $hash = $d[2];
-            $check = md5($d[0] . $d[1]);
+        try {
+            foreach ($data as $d) {
+                $pt = json_decode($d[0], true);
+                $et = json_decode($d[1], true);
+                $phealth = (float)$pt["h"];
+                $ehealth = (float)$et["h"];
+                if (!isset($ph)) {
+                    $ph = $phealth;
+                }
+                if (!isset($th)) {
+                    $th = $ehealth;
+                }
+                if ($phealth > $ph || $ehealth > $th) {
+                    $healthValidation = false;
+                    error_log("Anti-cheat: invalid health, potential healing");
+                    break;
+                }
+                $pdmg = (float)$pt["d"];
+                $edmg = (float)$et["d"];
+                if ($edmg < ($pdmg * .45)) {
+                    error_log("Anti-cheat: Enemy tank damage nerfed less than allowable offset");
+                    $dmgValidation = false;
+                    break;
+                }
+                //TODO add other validations as necessary, got lazy to validate other status, but the above are the important ones
+                $hash = $d[2];
+                $check = md5($d[0] . $d[1]);
 
-            error_log($hash . " vs " . $check);
-            error_log(var_export($pt, true));
-            error_log(var_export($et, true));
-            if($hash != $check){
-                $hashValidation = false;
-                error_log("Anti-cheat: hash mismatch");
-                break;
+                error_log($hash . " vs " . $check);
+                error_log(var_export($pt, true));
+                error_log(var_export($et, true));
+                if ($hash != $check) {
+                    $hashValidation = false;
+                    error_log("Anti-cheat: hash mismatch");
+                    break;
+                }
             }
         }
-        if(!$hashValidation || !$healthValidation || !$dmgValidation){
+        catch(Exception $e){
+            error_log("Validation failure");
+            error_log($e->getMessage());
+            $hashValidation = false;
+            $healthValidation = false;
+            $dmgValidation = false;
+        }
+        if(!$hashValidation || !$healthValidation || !$dmgValidation || count($data) < 2){
             $outcome = "loss";
         }
         error_log(var_export($data,true));
