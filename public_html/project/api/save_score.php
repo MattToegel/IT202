6ee77e42c0528ab88842b914eca8a89255a6cfc8
MattToegel <matthew.teogel@njit.edu>
2020-07-21 19:56:02 -0400

    added some anti cheat checks

--- public_html/project/api/save_score.php
+++ public_html/project/api/save_score.php
@@ -8,14 +8,49 @@ if(isset($_POST["score"]) && isset($_POST["outcome"])) {
         $outcome = Common::get($_POST, "outcome", "loss");
         $data = Common::get($_POST, "data", []);
         $data = json_decode($data, true);
+        $hashValidation = true;
+        $healthValidation = true;
+        $dmgValidation = true;
+        $ph = null;
+        $th = null;
         foreach($data as $d){
             $pt = json_decode($d[0], true);
             $et = json_decode($d[1], true);
+            $phealth = (float)$pt["h"];
+            $ehealth = (float)$et["h"];
+            if(!isset($ph)){
+                $ph = $phealth;
+            }
+            if(!isset($th)){
+                $th = $ehealth;
+            }
+            if($phealth > $ph || $ehealth > $th){
+                $healthValidation = false;
+                error_log("Anti-cheat: invalid health, potential healing");
+                break;
+            }
+            $pdmg = (float)$pt["d"];
+            $edmg = (float)$et["d"];
+            if($edmg < ($pdmg * .45)){
+                error_log("Anti-cheat: Enemy tank damage nerfed less than allowable offset");
+                $dmgValidation = false;
+                break;
+            }
+            //TODO add other validations as necessary, got lazy to validate other status, but the above are the important ones
             $hash = $d[2];
             $check = md5($d[0] . $d[1]);
+
             error_log($hash . " vs " . $check);
             error_log(var_export($pt, true));
             error_log(var_export($et, true));
+            if($hash != $check){
+                $hashValidation = false;
+                error_log("Anti-cheat: hash mismatch");
+                break;
+            }
+        }
+        if(!$hashValidation || !$healthValidation || !$dmgValidation){
+            $outcome = "loss";
         }
         error_log(var_export($data,true));
         if(Common::is_valid_game(($outcome=="win"))){
