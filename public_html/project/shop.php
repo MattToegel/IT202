4ef7b3ab8b97afa1e5f27c1494855e6063ae86f2
MattToegel <matthew.teogel@njit.edu>
2020-07-27 19:57:17 -0400

    updated shop complete purchase prompt

--- public_html/project/shop.php
+++ public_html/project/shop.php
@@ -150,7 +150,7 @@ $last_updated = Common::get($_SESSION, "last_sync", false);
         console.log(data);
         console.log(JSON.stringify(data));
         $.post("api/complete_purchase.php", {"order": JSON.stringify(data)}, function(data, status){
-            alert("Data: " + data + "\nStatus: " + status);
+            //alert("Data: " + data + "\nStatus: " + status);
             //reload the page
             window.location.replace("shop.php");
         });
