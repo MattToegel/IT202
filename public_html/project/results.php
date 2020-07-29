ad1a10e86644e552351f0c1f6e30712451fd5968
MattToegel <matthew.teogel@njit.edu>
2020-07-28 22:58:54 -0400

    working on questionnaire results

--- public_html/project/results.php
+++ public_html/project/results.php
@@ -1,10 +1,10 @@
 <?php
 include_once(__DIR__."/partials/header.partial.php");
 if(Common::is_logged_in()) {
-    $question_id = Common::get($_GET, "questionnaire_id", -1);
+    $survey_id = Common::get($_GET, "survey_id", -1);
     $stats = [];
-    if ($question_id > -1) {
-        $result = DBH::get_stats_for_questionnaire();
+    if ($survey_id > -1) {
+        $result = DBH::get_stats_for_questionnaire($survey_id);
         if(Common::get($result, "status", 400) == 200){
             $stats = Common::get($result, "data", []);
             error_log(var_export($stats, true));
