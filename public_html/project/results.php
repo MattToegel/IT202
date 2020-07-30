2f24a2261f8fbd7571de18568a290f6cfaa6a901
MattToegel <matthew.teogel@njit.edu>
2020-07-30 14:55:33 -0400

    blocking the grand total role from displaying

--- public_html/project/results.php
+++ public_html/project/results.php
@@ -27,11 +27,13 @@ if(Common::is_logged_in()) {
                 }
             ?>
             <?php foreach($question as $answer):?>
+                <?php if(Common::get($answer, "question_id", false) && Common::get($answer, "answer_id", false)):?>
                 <div class="list-group-item">
                     <?php echo Common::get($answer, "answer", "");?>
                     <?php echo Common::get($answer, "total", 0);?>
                     Max: <?php echo $max;?>
                 </div>
+                <?php endif;?>
             <?php endforeach;?>
         </div>
     <?php endforeach;?>
