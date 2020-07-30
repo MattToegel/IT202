e76aaa3382b3df97f95afa49dc908642cf017a86
MattToegel <matthew.teogel@njit.edu>
2020-07-30 14:21:51 -0400

    working on answer display

--- public_html/project/sql/queries/get_stats_for_questionnaire.sql
+++ public_html/project/sql/queries/get_stats_for_questionnaire.sql
@@ -1,9 +1,3 @@
-SELECT
-    question_id,
-    (select question from Questions where id = question_id limit 1) as question,
-    answer_id,
-    (select answer from Answers where id = answer_id limit 1) as answer,
-    count(user_id) as total from
-                                 (SELECT r.question_id, r.answer_id, a.answer, r.user_id from Answers a
-                                     left join Responses r on a.id = r.answer_id where r.questionnaire_id = :qid order by r.question_id) as T
-group by answer_id order by question_id
+SELECT question,r.question_id, answer, grouping(r.answer_id) `group`, count(r.user_id)
+from Questions q join Answers a on q.id = a.question_id join Responses r on r.answer_id = a.id
+where r.questionnaire_id = :qid and q.questionnaire_id = :qid group by r.question_id, r.answer_id with ROLLUP
