895aaecffd182b5e073a836815e9914c26068c72
MattToegel <matthew.teogel@njit.edu>
2020-07-30 18:37:18 -0400

    updated survey list and query to fetch all results but provide a value to determine if user took survey, this allows us to show results for surveys already taken

--- public_html/project/sql/queries/get_available_questionnaires.sql
+++ public_html/project/sql/queries/get_available_questionnaires.sql
@@ -1,7 +1,9 @@
-SELECT * from Questionnaires as q where
+SELECT *,
 (attempts_per_day > (SELECT COUNT(1)
 FROM Responses where user_id = :uid and questionnaire_id = q.id and date(created) = CURDATE())
 and
 q.use_max = 0)
 or
 (q.use_max = 1 and q.max_attempts > (select COUNT(1) from Responses where user_id = :uid and questionnaire_id = q.id))
+as available
+from Questionnaires as q
