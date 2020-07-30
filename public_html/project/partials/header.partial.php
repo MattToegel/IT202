41692fb361c474de4f6383fa376ff54e175be8af
MattToegel <matthew.teogel@njit.edu>
2020-07-30 18:53:43 -0400

    renamed home to dashboard

--- public_html/project/partials/header.partial.php
+++ public_html/project/partials/header.partial.php
@@ -13,9 +13,12 @@ $logged_in = Common::is_logged_in(false);
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <ul class="navbar-nav mr-auto">
+        <li class="nav-item">
+            <a class="nav-link" href="<?php echo Common::url_for("index");?>">Home</a>
+        </li>
         <?php if($logged_in):?>
         <li class="nav-item">
-            <a class="nav-link" href="<?php echo Common::url_for("home");?>">Home</a>
+            <a class="nav-link" href="<?php echo Common::url_for("dashboard");?>">Dashboard</a>
         </li>
         <li class="nav-item">
             <a class="nav-link" href="<?php echo Common::url_for("game");?>">Game</a>
