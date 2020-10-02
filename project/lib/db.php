251397a06cc6fcf17242ea46546a7da45d199b55
MattToegel <matthew.toegel@njit.edu>
2020-10-02 23:18:00 +0000

    fixed path for db.php

--- project/lib/db.php
+++ project/lib/db.php
@@ -12,7 +12,7 @@ function getDB(){
         try{
             //__DIR__ helps get the correct path regardless of where the file is being called from
             //it gets the absolute path to this file, then we append the relative url (so up a directory and inside lib)
-            require_once(__DIR__. "/../lib/config.php");//pull in our credentials
+            require_once(__DIR__. "/config.php");//pull in our credentials
             //use the variables from config to populate our connection
             $connection_string = "mysql:host=$dbhost;dbname=$dbdatabase;charset=utf8mb4";
             //using the PDO connector create a new connect to the DB
