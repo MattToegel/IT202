dc0f84c487b2368b53d7c67102e5dc08b0dca437
MattToegel <matthew.toegel@njit.edu>
2020-10-03 00:27:55 +0000

    fixed typos in nav

--- project/partials/nav.php
+++ project/partials/nav.php
@@ -6,11 +6,10 @@ require_once(__DIR__ . "/../lib/helpers.php");
 <ul>
     <li><a href="home.php">Home</a></li>
     <?php if (!is_logged_in()): ?>
-        <li><a href="sample_login.php">Login</a></li>
-        <li><a href="sample_reg.php">Register</a></li>
+        <li><a href="login.php">Login</a></li>
+        <li><a href="register.php">Register</a></li>
     <?php endif; ?>
     <?php if (is_logged_in()): ?>
         <li><a href="logout.php">Logout</a></li>
     <?php endif; ?>
 </ul>
-?>
