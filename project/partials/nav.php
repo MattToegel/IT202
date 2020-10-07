fc0bd19bc0e40ff511dd0a88fe9cdd6c85252146
MattToegel <matthew.toegel@njit.edu>
2020-10-06 23:00:31 -0400

    Added profile link to nav. Added username field to register.php Added duplicate email/username check to register.php

--- project/partials/nav.php
+++ project/partials/nav.php
@@ -9,6 +9,7 @@ require_once(__DIR__ . "/../lib/helpers.php");
         <li><a href="register.php">Register</a></li>
     <?php endif; ?>
     <?php if (is_logged_in()): ?>
+        <li><a href="profile.php">Profile</a></li>
         <li><a href="logout.php">Logout</a></li>
     <?php endif; ?>
 </ul>
--- project/register.php
+++ project/register.php
@@ -1,18 +1,11 @@
 <?php require_once(__DIR__ . "/partials/nav.php"); ?>
-<form method="POST">
-    <label for="email">Email:</label>
-    <input type="email" id="email" name="email" required/>
-    <label for="p1">Password:</label>
-    <input type="password" id="p1" name="password" required/>
-    <label for="p2">Confirm Password:</label>
-    <input type="password" id="p2" name="confirm" required/>
-    <input type="submit" name="register" value="Register"/>
-</form>
+
 <?php
 if (isset($_POST["register"])) {
     $email = null;
     $password = null;
     $confirm = null;
+    $username = null;
     if (isset($_POST["email"])) {
         $email = $_POST["email"];
     }
@@ -21,6 +14,9 @@ if (isset($_POST["register"])) {
     }
     if (isset($_POST["confirm"])) {
         $confirm = $_POST["confirm"];
+    }
+    if (isset($_POST["username"])) {
+        $username = $_POST["username"];
     }
     $isValid = true;
     //check if passwords match on the server side
@@ -37,13 +33,13 @@ if (isset($_POST["register"])) {
     //TODO other validation as desired, remember this is the last line of defense
     if ($isValid) {
         $hash = password_hash($password, PASSWORD_BCRYPT);
-        
+
         $db = getDB();
         if (isset($db)) {
             //here we'll use placeholders to let PDO map and sanitize our data
-            $stmt = $db->prepare("INSERT INTO Users(email, password) VALUES(:email, :password)");
+            $stmt = $db->prepare("INSERT INTO Users(email, username, password) VALUES(:email,:username, :password)");
             //here's the data map for the parameter to data
-            $params = array(":email" => $email, ":password" => $hash);
+            $params = array(":email" => $email, ":username" => $username, ":password" => $hash);
             $r = $stmt->execute($params);
             //let's just see what's returned
             echo "db returned: " . var_export($r, true);
@@ -52,7 +48,12 @@ if (isset($_POST["register"])) {
                 echo "<br>Welcome! You successfully registered, please login.";
             }
             else {
-                echo "uh oh something went wrong: " . var_export($e, true);
+                if ($e[0] == "23000") {//code for duplicate entry
+                    echo "<br>Either username or email is already registered, please try again";
+                }
+                else {
+                    echo "uh oh something went wrong: " . var_export($e, true);
+                }
             }
         }
     }
@@ -61,3 +62,14 @@ if (isset($_POST["register"])) {
     }
 }
 ?>
+<form method="POST">
+    <label for="email">Email:</label>
+    <input type="email" id="email" name="email" required value="<?php echo $email; ?>"/>
+    <label for="user">Username:</label>
+    <input type="text" id="user" name="username" required maxlength="60" value="<?php echo $username; ?>"/>
+    <label for="p1">Password:</label>
+    <input type="password" id="p1" name="password" required/>
+    <label for="p2">Confirm Password:</label>
+    <input type="password" id="p2" name="confirm" required/>
+    <input type="submit" name="register" value="Register"/>
+</form>
