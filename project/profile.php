fc31513ea2bea96ca02f1455d097c11b01b9eb76
MattToegel <matthew.toegel@njit.edu>
2020-10-07 09:50:48 -0400

    Added login check to profile.php

--- project/profile.php
+++ project/profile.php
@@ -3,6 +3,10 @@
 //Note: we have this up here, so our update happens before our get/fetch
 //that way we'll fetch the updated data and have it correctly reflect on the form below
 //As an exercise swap these two and see how things change
+if (!is_logged_in()) {
+    //this will redirect to login and kill the rest of this script (prevent it from executing)
+    die(header("Location: login.php"));
+}
 
 $db = getDB();
 //save data if we submitted the form
