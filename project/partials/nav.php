36d00ab8fca3cf0dc1fc8c00d579270f5505ed9b
MattToegel <matthew.toegel@njit.edu>
2020-10-08 17:14:46 -0400

    working on css

--- project/partials/nav.php
+++ project/partials/nav.php
@@ -1,8 +1,9 @@
+<link rel="stylesheet" href="static/css/styles.css">
 <?php
 //we'll be including this on most/all pages so it's a good place to include anything else we want on those pages
 require_once(__DIR__ . "/../lib/helpers.php");
 ?>
-<ul>
+<ul class="nav">
     <li><a href="home.php">Home</a></li>
     <?php if (!is_logged_in()): ?>
         <li><a href="login.php">Login</a></li>
