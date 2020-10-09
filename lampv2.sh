da5d8f283f24a833bd93e950aeb7432a17937177
MattToegel <Matthew.toegel@njit.edu>
2020-10-09 19:49:03 -0400

    Update lampv2.sh

--- lampv2.sh
+++ lampv2.sh
@@ -42,6 +42,7 @@ sudo chmod 600 /swapfile
 sudo mkswap /swapfile
 sudo swapon /swapfile
 echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
+sudo sysctl vm.swappiness=10
 
 #install apache2
 sudo apt install -y apache2 apache2-utils
