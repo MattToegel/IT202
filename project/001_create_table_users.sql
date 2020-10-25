59848d2aa81ec76a5f2a838c9a82170422cf5723
MattToegel <matthew.toegel@njit.edu>
2020-10-25 00:48:33 +0000

    fixed misplaced file from video

--- project/001_create_table_users.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS `Users` (
-                                       `id` INT NOT NULL AUTO_INCREMENT
-    ,`email` VARCHAR(100) NOT NULL
-    ,`password` VARCHAR(60) NOT NULL
-    ,`created` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
-    ,PRIMARY KEY (`id`)
-    ,UNIQUE (`email`)
-)
