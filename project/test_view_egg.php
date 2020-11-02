b7b424a6776d530d8ec1f511c8d7667d792d71fb
MattToegel <matthew.toegel@njit.edu>
2020-11-02 10:28:19 -0500

    cleaned up view sample, added join with user's to get name

--- project/test_view_egg.php
+++ project/test_view_egg.php
@@ -17,7 +17,7 @@ if (isset($_GET["id"])) {
 $result = [];
 if (isset($id)) {
     $db = getDB();
-    $stmt = $db->prepare("SELECT * FROM F20_Eggs where id = :id");
+    $stmt = $db->prepare("SELECT Eggs.id,name,state,base_rate,mod_min,mod_max,next_stage_time, user_id, user.username FROM F20_Eggs as Eggs JOIN Users on Eggs.user_id = Users.id where id = :id");
     $r = $stmt->execute([":id" => $id]);
     $result = $stmt->fetch(PDO::FETCH_ASSOC);
 }
@@ -34,31 +34,11 @@ if (isset($id)) {
                 <div>Modifier: <?php safer_echo($result["mod_min"]); ?> - <?php safer_echo($result["mod_max"]); ?></div>
                 <div>Current State: <?php getState($result["state"]); ?></div>
                 <div>Next Stage: <?php safer_echo($result["next_stage_time"]); ?></div>
+                <div>Owned by: <?php safer_echo($result["username"]); ?></div>
             </div>
         </div>
     </div>
 <?php else: ?>
     <p>Error looking up id...</p>
 <?php endif; ?>
-
-    <form method="POST">
-        <label>Name</label>
-        <input name="name" placeholder="Name" value="<?php echo $result["name"]; ?>"/>
-        <label>State</label>
-        <select name="state" value="<?php echo $result["state"]; ?>">
-            <option value="0" <?php echo($result["state"] == "0" ? 'selected="selected"' : ''); ?>>Incubating</option>
-            <option value="1" <?php echo($result["state"] == "1" ? 'selected="selected"' : ''); ?>>Hatching</option>
-            <option value="2" <?php echo($result["state"] == "2" ? 'selected="selected"' : ''); ?>>Hatched</option>
-            <option value="3" <?php echo($result["state"] == "3" ? 'selected="selected"' : ''); ?>>Expired</option>
-        </select>
-        <label>Base Rate</label>
-        <input type="number" min="1" name="base_rate" value="<?php echo $result["base_rate"]; ?>"/>
-        <label>Mod Min</label>
-        <input type="number" min="1" name="mod_min" value="<?php echo $result["mod_min"]; ?>"/>
-        <label>Mod Max</label>
-        <input type="number" min="1" name="mod_max" value="<?php echo $result["mod_max"]; ?>"/>
-        <input type="submit" name="save" value="Update"/>
-    </form>
-
-
 <?php require(__DIR__ . "/partials/flash.php");
