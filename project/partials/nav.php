aab1bd8f2f68a84cf6db5240163ecdb91058fc30
MattToegel <matthew.toegel@njit.edu>
2020-11-02 10:33:28 -0500

    added Admin navbar item for test files

--- project/partials/nav.php
+++ project/partials/nav.php
@@ -10,6 +10,10 @@ require_once(__DIR__ . "/../lib/helpers.php");
             <li><a href="login.php">Login</a></li>
             <li><a href="register.php">Register</a></li>
         <?php endif; ?>
+        <?php if (has_role("Admin")): ?>
+            <li><a href="test_create_egg.php">Create Egg</a></li>
+            <li><a href="test_list_egg.php">View Eggs</a></li>
+        <?php endif; ?>
         <?php if (is_logged_in()): ?>
             <li><a href="profile.php">Profile</a></li>
             <li><a href="logout.php">Logout</a></li>
