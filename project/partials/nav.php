52f9c5266322b610f33824457edb775c1242ed65
MattToegel <matthew.toegel@njit.edu>
2020-11-03 22:41:18 -0500

    added new links to navbar for incubator

--- project/partials/nav.php
+++ project/partials/nav.php
@@ -13,6 +13,8 @@ require_once(__DIR__ . "/../lib/helpers.php");
         <?php if (has_role("Admin")): ?>
             <li><a href="test_create_egg.php">Create Egg</a></li>
             <li><a href="test_list_egg.php">View Eggs</a></li>
+            <li><a href="test_create_incubator.php">Create Incubator</a></li>
+            <li><a href="test_list_incubators.php">View Incubator</a></li>
         <?php endif; ?>
         <?php if (is_logged_in()): ?>
             <li><a href="profile.php">Profile</a></li>
