5bd5a4d583383c409a65245d7ddd2779486193f4
MattToegel <matthew.toegel@njit.edu>
2020-11-03 22:37:40 -0500

    added incubator view

---
+++ project/test_view_incubator.php
@@ -0,0 +1,48 @@
+<?php require_once(__DIR__ . "/partials/nav.php"); ?>
+<?php
+if (!has_role("Admin")) {
+    //this will redirect to login and kill the rest of this script (prevent it from executing)
+    flash("You don't have permission to access this page");
+    die(header("Location: login.php"));
+}
+?>
+<?php
+//we'll put this at the top so both php block have access to it
+if (isset($_GET["id"])) {
+    $id = $_GET["id"];
+}
+?>
+<?php
+//fetching
+$result = [];
+if (isset($id)) {
+    $db = getDB();
+    $stmt = $db->prepare("SELECT incu.id,incu.name,base_rate,mod_min,mod_max, Users.username, Egg.name as egg FROM F20_Incubators as incu JOIN Users on incu.user_id = Users.id JOIN F20_Eggs Egg on Egg.id = incu.egg_id where incu.id = :id");
+    $r = $stmt->execute([":id" => $id]);
+    $result = $stmt->fetch(PDO::FETCH_ASSOC);
+    if (!$result) {
+        $e = $stmt->errorInfo();
+        flash($e[2]);
+    }
+}
+?>
+    <h3>View Incubator</h3>
+<?php if (isset($result) && !empty($result)): ?>
+    <div class="card">
+        <div class="card-title">
+            <?php safer_echo($result["name"]); ?>
+        </div>
+        <div class="card-body">
+            <div>
+                <p>Stats</p>
+                <div>Rate: <?php safer_echo($result["base_rate"]); ?></div>
+                <div>Modifier: <?php safer_echo($result["mod_min"]); ?> - <?php safer_echo($result["mod_max"]); ?></div>
+                <div>Egg: <?php safer_echo($result["egg"]); ?></div>
+                <div>Owned by: <?php safer_echo($result["username"]); ?></div>
+            </div>
+        </div>
+    </div>
+<?php else: ?>
+    <p>Error looking up id...</p>
+<?php endif; ?>
+<?php require(__DIR__ . "/partials/flash.php");
