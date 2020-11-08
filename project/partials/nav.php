b4574e9dfceadfa9c41d6e029dd84d95ea222028
MattToegel <matthew.toegel@njit.edu>
2020-11-08 13:23:31 -0500

    fixed typo in file name

--- project/partials/nav.php
+++ project/partials/nav.php
@@ -14,7 +14,7 @@ require_once(__DIR__ . "/../lib/helpers.php");
             <li><a href="<?php echo getURL("test/test_create_egg.php"); ?>">Create Egg</a></li>
             <li><a href="<?php echo getURL("test/test_list_egg.php"); ?>">View Eggs</a></li>
             <li><a href="<?php echo getURL("test/test_create_incubator.php"); ?>">Create Incubator</a></li>
-            <li><a href="<?php echo getURL("test/test_list_incrubators.php"); ?>">View Incubator</a></li>
+            <li><a href="<?php echo getURL("test/test_list_incubators.php"); ?>">View Incubator</a></li>
         <?php endif; ?>
         <?php if (is_logged_in()): ?>
             <li><a href="profile.php">Profile</a></li>
