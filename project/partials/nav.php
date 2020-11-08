9e19d0e26ff1b75a5bf457561e2fd41403122190
MattToegel <matthew.toegel@njit.edu>
2020-11-08 18:32:32 +0000

    fixed path issues

--- project/partials/nav.php
+++ project/partials/nav.php
@@ -1,11 +1,12 @@
-<link rel="stylesheet" href="<?php echo getURL("static/css/styles.css"); ?>">
 <?php
 //we'll be including this on most/all pages so it's a good place to include anything else we want on those pages
 require_once(__DIR__ . "/../lib/helpers.php");
 ?>
+<link rel="stylesheet" href="<?php echo getURL("static/css/styles.css"); ?>">
+
 <nav>
     <ul class="nav">
-        <li><a href="home.php">Home</a></li>
+        <li><a href="<?php echo getURL("home.php");?>">Home</a></li>
         <?php if (!is_logged_in()): ?>
             <li><a href="<?php echo getURL("login.php"); ?>">Login</a></li>
             <li><a href="<?php echo getURL("register.php"); ?>">Register</a></li>
