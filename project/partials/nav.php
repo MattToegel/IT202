f0d4ff6d196386078b966f08172a43c5003eaf01
MattToegel <matthew.toegel@njit.edu>
2020-11-10 20:29:06 -0500

    added cdn links for boostrap css, jquery, and related js. Started updating nav ui

--- project/partials/nav.php
+++ project/partials/nav.php
@@ -2,26 +2,42 @@
 //we'll be including this on most/all pages so it's a good place to include anything else we want on those pages
 require_once(__DIR__ . "/../lib/helpers.php");
 ?>
-<link rel="stylesheet" href="<?php echo getURL("static/css/styles.css"); ?>">
+<!-- CSS -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
+      integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
 
-<nav>
-    <ul class="nav">
-        <li><a href="<?php echo getURL("home.php");?>">Home</a></li>
+<!-- jQuery and JS bundle w/ Popper.js -->
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
+        crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx"
+        crossorigin="anonymous"></script>
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <ul class="navbar-nav mr-auto">
+        <li class="nav-item"><a class="nav-link" href="<?php echo getURL("home.php"); ?>">Home</a></li>
         <?php if (!is_logged_in()): ?>
-            <li><a href="<?php echo getURL("login.php"); ?>">Login</a></li>
-            <li><a href="<?php echo getURL("register.php"); ?>">Register</a></li>
+            <li class="nav-item"><a class="nav-link" href="<?php echo getURL("login.php"); ?>">Login</a></li>
+            <li class="nav-item"><a class="nav-link" href="<?php echo getURL("register.php"); ?>">Register</a></li>
         <?php endif; ?>
         <?php if (has_role("Admin")): ?>
-            <li><a href="<?php echo getURL("test/test_create_egg.php"); ?>">Create Egg</a></li>
-            <li><a href="<?php echo getURL("test/test_list_egg.php"); ?>">View Eggs</a></li>
-            <li><a href="<?php echo getURL("test/test_create_incubator.php"); ?>">Create Incubator</a></li>
-            <li><a href="<?php echo getURL("test/test_list_incubators.php"); ?>">View Incubator</a></li>
+            <li class="nav-item"><a class="nav-link" href="<?php echo getURL("test/test_create_egg.php"); ?>">Create
+                    Egg</a></li>
+            <li class="nav-item"><a class="nav-link" href="<?php echo getURL("test/test_list_egg.php"); ?>">View
+                    Eggs</a></li>
+            <li class="nav-item"><a class="nav-link" href="<?php echo getURL("test/test_create_incubator.php"); ?>">Create
+                    Incubator</a>
+            </li>
+            <li class="nav-item"><a class="nav-link" href="<?php echo getURL("test/test_list_incubators.php"); ?>">View
+                    Incubator</a>
+            </li>
         <?php endif; ?>
         <?php if (is_logged_in()): ?>
-		<li><a href="<?php echo getURL("shop.php");?>">Shop</a></li>
-            <li><a href="<?php echo getURL("profile.php"); ?>">Profile</a></li>
-            <li><a href="<?php echo getURL("logout.php"); ?>">Logout</a></li>
+            <li class="nav-item"><a class="nav-link" href="<?php echo getURL("shop.php"); ?>">Shop</a></li>
+            <li class="nav-item"><a class="nav-link" href="<?php echo getURL("profile.php"); ?>">Profile</a></li>
+            <li class="nav-item"><a class="nav-link" href="<?php echo getURL("logout.php"); ?>">Logout</a></li>
         <?php endif; ?>
     </ul>
-    <span>Balance: <?php echo getBalance();?></span>
+    <span class="navbar-text">Balance: <?php echo getBalance(); ?></span>
 </nav>
