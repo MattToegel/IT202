70fd304c9d3c5736c35410ed65a1387b3add793f
MattToegel <matthew.toegel@njit.edu>
2020-11-14 22:49:01 +0000

    working on survey view

--- project/survey.php
+++ project/survey.php
@@ -12,15 +12,28 @@ if (isset($_GET["id"])) {
     $db = getDB();
     $stmt = $db->prepare("SELECT q.id as GroupId, q.id as QuestionId, q.question, s.id as SurveyId, s.name as SurveyName, a.id as AnswerId, a.answer FROM F20_Surveys as s JOIN F20_Questions as q on s.id = q.survey_id JOIN F20_Answers as a on a.question_id = q.id WHERE :id not in (SELECT user_id from F20_Responses where user_id = :id and survey_id = :survey_id) and s.id = :survey_id");
     $r = $stmt->execute([":id" => get_user_id(), ":survey_id" => $sid]);
-    if ($r) {
+   $name = "";
+	$questions = [];
+	 if ($r) {
         $results = $stmt->fetchAll(PDO::FETCH_GROUP);
-        echo "<pre>" . var_export($results, true) . "</pre>";
-        echo "<br>";
-        foreach ($results as $group) {
+       // echo "<pre>" . var_export($results, true) . "</pre>";
+       // echo "<br>";
+        foreach ($results as $index=>$group) {
             foreach ($group as $details) {
-                echo "<br>" . $group["question"] . " " . $details["answer"] . "<br>";
+		$answer = ["answerId"=>$details["AnswerId"], "answer"=>$details["answer"]];
+		if(in_array($details["QuestionId"], $questions)){
+			//array_push($questions[$details["QuestionId"]["answers"], ["answerId"=>$details["AnswerId"], "answer"=>$details["answer"]]);
+		}
+		else{
+			$questions[$details["QuestionId"]]["question"] = $details["question"];
+			$questions[$details["QuestionId"]]["answers"] = [];
+		}
+		array_push($questions[$details["QuestionId"]]["answers"], $answer);
+                //echo "<br>" . $details["question"] . " " . $details["answer"] . "<br>";
             }
         }
+			echo "<pre>" . var_export($questions, true) . "</pre>";
+
     }
     else {
         flash("There was a problem fetching the survey: " . var_export($stmt->errorInfo(), true), "danger");
