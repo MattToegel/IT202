cf7b097cbd4fa989782b2ffd1354e717413bf9bd
MattToegel <matthew.toegel@njit.edu>
2020-11-22 17:57:51 -0500

    added login check for mycart

--- project/my_cart.php
+++ project/my_cart.php
@@ -1,5 +1,12 @@
 <?php require_once(__DIR__ . "/partials/nav.php"); ?>
+
 <?php
+if (!is_logged_in()) {
+    //this will redirect to login and kill the rest of this script (prevent it from executing)
+    flash("You must be logged in to access this page");
+    die(header("Location: login.php"));
+}
+
 $db = getDB();
 if(isset($_POST["update"])){
     $stmt = $db->prepare("UPDATE F20_Cart set quantity = :q where id = :id");
