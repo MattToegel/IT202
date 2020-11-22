670a66c8802fa40446315bcc1adc835f0420d64c
MattToegel <matthew.toegel@njit.edu>
2020-11-22 14:24:03 -0500

    fixed swapped created/modified

--- project/sql/010_create_table_answers.sql
+++ project/sql/010_create_table_answers.sql
@@ -3,8 +3,8 @@ CREATE TABLE IF NOT EXISTS F20_Answers
     id          int auto_increment,
     answer      varchar(120) not null,
 
-    modified    TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    created     TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP on update current_timestamp,
+    modified    TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP on update current_timestamp,
+    created     TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP ,
     question_id int,
     primary key (id),
     FOREIGN KEY (question_id) REFERENCES F20_Questions (id)
