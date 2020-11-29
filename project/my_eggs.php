f9b96fc8af3d858a6b4d5f77bf61e86c5191ac79
MattToegel <matthew.toegel@njit.edu>
2020-11-29 18:11:31 -0500

    working on pagination

--- project/my_eggs.php
+++ project/my_eggs.php
@@ -6,11 +6,29 @@ if (!is_logged_in()) {
     flash("You must be logged in to access this page");
     die(header("Location: login.php"));
 }
+//https://www.digitalocean.com/community/tutorials/how-to-implement-pagination-in-mysql-with-php-on-ubuntu-18-04
+$page = 1;
+$per_page = 10;
+if(isset($_GET["page"])){
+    try {
+        $page = (int)$_GET["page"];
+    }
+    catch(Exception $e){
 
+    }
+}
 $db = getDB();
-
-$stmt = $db->prepare("SELECT e.*, i.name as inc from F20_Eggs e LEFT JOIN F20_Incubators i on e.id = i.egg_id where e.user_id = :id");
+$stmt = $db->prepare("SELECT count(*) as total from F20_Eggs e LEFT JOIN F20_Incubators i on e.id = i.egg_id where e.user_id = :id");
 $stmt->execute([":id"=>get_user_id()]);
+$result = $stmt->fetch(PDO::FETCH_ASSOC);
+$total = 0;
+if($result){
+    $total = (int)$result["total"];
+}
+$total_pages = ceil($total / $per_page);
+$offset = ($page-1) * $per_page;
+$stmt = $db->prepare("SELECT e.*, i.name as inc from F20_Eggs e LEFT JOIN F20_Incubators i on e.id = i.egg_id where e.user_id = :id LIMIT :offset, :count");
+$stmt->execute([":id"=>get_user_id(), ":offset"=>$offset, ":count"=>$per_page]);
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 ?>
     <div class="container-fluid">
@@ -50,5 +68,18 @@ $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 <?php endif;?>
     </div>
     </div>
+        <nav aria-label="My Eggs">
+            <ul class="pagination justify-content-center">
+                <li class="page-item <?php echo $page < 1?"disabled":"";?>">
+                    <a class="page-link" href="#" tabindex="-1">Previous</a>
+                </li>
+                <?php for($i = 0; $i < $total_pages; $i++):?>
+                <li class="page-item <?php echo ($page-1) == $i?"active":"";?>"><a class="page-link" href="#"><?php echo ($i+1);?></a></li>
+                <?php endfor; ?>
+                <li class="page-item <?php echo $page >= $total_pages?"disabled":"";?>>">
+                    <a class="page-link" href="#">Next</a>
+                </li>
+            </ul>
+        </nav>
     </div>
 <?php require(__DIR__ . "/partials/flash.php");
