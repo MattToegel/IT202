97ef7d526250b31868178ccdb24b80f65367e8fc
MattToegel <Matthew.toegel@njit.edu>
2020-12-09 20:51:01 -0500

    Update get_available_questionnaires.sql

--- public_html/project/sql/queries/get_available_questionnaires.sql
+++ public_html/project/sql/queries/get_available_questionnaires.sql
@@ -1,9 +1,1 @@
-SELECT *,
-(attempts_per_day > (SELECT COUNT(1)
-FROM Responses where user_id = :uid and questionnaire_id = q.id and date(created) = CURDATE())
-and
-q.use_max = 0)
-or
-(q.use_max = 1 and q.max_attempts > (select COUNT(1) from Responses where user_id = :uid and questionnaire_id = q.id))
-as available
-from Questionnaires as q
+SELECT * FROM (SELECT *, (attempts_per_day > (SELECT COUNT(1) FROM Responses where user_id = 1 and questionnaire_id = q.id and date(created) = CURDATE()) and q.use_max = 0) or (q.use_max = 1 and q.max_attempts > (select COUNT(1) from Responses where user_id = 1 and questionnaire_id = q.id)) as available from Questionnaires as q) as T WHERE available > 0
