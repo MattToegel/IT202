615a228089ade4d2f6691dd23436bc4ce3a685d6
MattToegel <matthew.toegel@njit.edu>
2021-03-30 21:26:53 +0000

    adding projectp2

---
+++ projectp2/partials/nav.php
@@ -0,0 +1,23 @@
+<link rel="stylesheet" href="static/css/styles.css">
+<?php
+//we'll be including this on most/all pages so it's a good place to include anything else we want on those pages
+require_once(__DIR__ . "/../lib/helpers.php");
+?>
+<nav>
+<ul class="nav">
+    <li><a href="home.php">Home</a></li>
+    <?php if(!is_logged_in()):?>
+    <li><a href="login.php">Login</a></li>
+    <li><a href="register.php">Register</a></li>
+    <?php endif;?>
+    <?php if(has_role("Admin")):?>
+         <li><a href="test_create_tank.php">Create Tank</a></li>
+         <!-- can't quite do this due to the query param-->
+          <li><a href="test_edit_tank.php?tankId=1">Edit Tank</a></li>
+    <?php endif;?>
+    <?php if(is_logged_in()):?>
+        <li><a href="profile.php">Profile</a></li>
+    <li><a href="logout.php">Logout</a></li>
+    <?php endif; ?>
+</ul>
+</nav>
