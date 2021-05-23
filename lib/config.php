4df1e8efacf1e32549af8e01db605bf4ab76e79f
MattToegel <matthew.toegel@njit.edu>
2021-05-23 17:11:45 -0400

    updated .gitignore to ignore .env file, updated config for local .env loading, added readme to public_html

--- lib/config.php
+++ lib/config.php
@@ -1,7 +1,15 @@
 <?php
-$cleardb_url      = parse_url(getenv("DB_URL"));
-$dbhost   = $cleardb_url["host"];
-$dbuser = $cleardb_url["user"];
-$dbpass = $cleardb_url["pass"];
-$dbdatabase       = substr($cleardb_url["path"],1);
+$ini = parse_ini_file(".env");
+if($ini && isset($ini["DB_URL"])){
+    //load local .env file
+    $db_url = parse_url($ini["DB_URL"]);
+}
+else{
+    //load from heroku env variables
+    $db_url      = parse_url(getenv("DB_URL"));
+}
+$dbhost   = $db_url["host"];
+$dbuser = $db_url["user"];
+$dbpass = $db_url["pass"];
+$dbdatabase       = substr($db_url["path"],1);
 ?>
