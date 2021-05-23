d70a852e1684b092d89419f66528873dd5fa10ab
MattToegel <matthew.toegel@njit.edu>
2021-05-23 14:14:10 -0400

    updating default fetch mode and test example

--- lib/db.php
+++ lib/db.php
@@ -18,6 +18,9 @@ function getDB(){
             //using the PDO connector create a new connect to the DB
             //if no error occurs we're connected
             $db = new PDO($connection_string, $dbuser, $dbpass);
+	    //the default fetch mode is FETCH_BOTH which returns the data as both an indexed array and associative array
+	    //we'll override the default here so it's always fetched as an associative array
+ 	    $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 	}
    	catch(Exception $e){
             var_export($e);
