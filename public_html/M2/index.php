0ca2523eeb16c5e1f0500c092c727f2438592aef
MattToegel <matthew.toegel@njit.edu>
2021-05-28 16:51:37 -0400

    updated indexes to add back button and slightly better landing page

--- public_html/M2/index.php
+++ public_html/M2/index.php
@@ -4,5 +4,6 @@
     <li><a href="loops.php">PHP and JS Loops</a>
     <li><a href="floats.php">Important: Dealing with Floats/Decimals</a></li>
     <li><a href="practice.php">Practice</a></li>
+    <li><a href="..">Back</a></li>
 </ul>
 </ul>
