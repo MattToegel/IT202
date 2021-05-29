84b480781cdd3ae5744f0d9a5615e183dbc9be57
MattToegel <matthew.toegel@njit.edu>
2021-05-29 15:25:26 -0400

    updated problem 2 layout

--- public_html/M2/problem2.php
+++ public_html/M2/problem2.php
@@ -2,15 +2,47 @@
 $a1 = [10.001, 11.591, 0.011, 5.991, 16.121, 0.131, 100.981, 1.001];
 $a2 = [1.99, 1.99, 0.99, 1.99, 0.99, 1.99, 0.99, 0.99];
 $a3 = [0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01];
+$a4 = [10.01, -12.22, 0.23, 19.20, -5.13, 3.12];
 function getTotal($arr) {
+    echo "<br>Processing Array:<br><pre>" . var_export($arr, true) . "</pre>";
     $total = 0.00;
     //TODO do adding here
     //TODO do rounding stuff here
-    return $total;
+    echo "The total is " . var_export($total, true);
 }
 echo "Problem 2: Adding Floats<br>";
-echo "The total of Array 1 is " . var_export(getTotal($a1), true);
-echo "<br>";
-echo "The total of Array 2 is " . var_export(getTotal($a2), true);
-echo "<br>";
-echo "The total of Array 3 is " . var_export(getTotal($a3), true);
+?>
+<table>
+    <thread>
+        <th>A1</th>
+        <th>A2</th>
+        <th>A3</th>
+        <th>A4</th>
+    </thread>
+    <tbody>
+        <tr>
+            <td>
+                <?php getTotal($a1) ?>
+            </td>
+            <td>
+                <?php getTotal($a2) ?>
+            </td>
+            <td>
+                <?php getTotal($a3) ?>
+            </td>
+            <td>
+                <?php getTotal($a4); ?>
+            </td>
+        </tr>
+</table>
+<style>
+    table {
+        border-spacing: 2em 3em;
+        border-collapse: separate;
+    }
+
+    td {
+        border-right: solid 1px black;
+        border-left: solid 1px black;
+    }
+</style>
