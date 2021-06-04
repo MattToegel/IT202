c7b325cfde94f317f3e846a0f5b73afbeef822b3
Rubab-hub <85041284+Rubab-hub@users.noreply.github.com>
2021-06-03 21:14:42 -0400

    Update problem1.php

--- public_html/M2/problem1.php
+++ public_html/M2/problem1.php
@@ -4,11 +4,15 @@ $a2 = [0, 1, 3, 5, 7, 9, 2, 4, 6, 8, 10];
 $a3 = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0];
 $a4 = [0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 10, 10];
 function processArray($arr) {
-    //use the $arr variable to iterate over
+    public static void main(String args[]) {
+	int n = 10;
     echo "<br>Processing Array:<br><pre>" . var_export($arr, true) . "</pre>";
     echo "<br>Odds output:<br>";
-    //TODO add logic here to echo out only odd values
-
+    System.out.print("Odd Numbers from 1 to "+n+" are: ");
+	for (int i = 1; i <= n; i++) {
+	   if (i % 2 != 0) {
+		System.out.print(i + " ");
+       }
 }
 echo "Problem 1: Odd Output<br>";
 ?>
