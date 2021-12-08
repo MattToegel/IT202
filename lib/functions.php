a84b25b08f5a5cfb8b78faa18d28b5b68de7f17b
MattToegel <matthew.toegel@njit.edu>
2021-12-07 23:17:29 -0500

    added/fixed ranking for unique user ranks

--- lib/functions.php
+++ lib/functions.php
@@ -648,13 +648,21 @@ function elog($data)
 function get_top_scores_for_comp($comp_id, $limit = 10)
 {
     $db = getDB();
-    $stmt = $db->prepare(
+    //below if a user can win more than one place
+    /*$stmt = $db->prepare(
         "SELECT score, s.created, username, u.id as user_id FROM BGD_Scores s 
     JOIN BGD_UserComps uc on uc.user_id = s.user_id 
     JOIN BGD_Competitions c on c.id = uc.competition_id
     JOIN Users u on u.id = s.user_id WHERE c.id = :cid AND s.score >= c.min_score AND s.created 
     BETWEEN uc.created AND c.expires ORDER BY s.score desc LIMIT :limit"
-    );
+    );*/
+    //Below if a user can't win more than one place
+    $stmt = $db->prepare("SELECT * FROM (SELECT s.user_id, s.score,s.created, a.id as account_id, DENSE_RANK() OVER (PARTITION BY s.user_id ORDER BY s.score desc) as `rank` FROM BGD_Scores s
+    JOIN BGD_UserComps uc on uc.user_id = s.user_id
+    JOIN BGD_Competitions c on uc.competition_id = c.id
+    JOIN BGD_Accounts a on a.user_id = s.user_id
+    WHERE c.id = :cid AND s.created BETWEEN uc.created AND c.expires
+    )as t where `rank` = 1 ORDER BY score desc LIMIT :limit");
     $scores = [];
     try {
         $stmt->bindValue(":cid", $comp_id, PDO::PARAM_INT);
