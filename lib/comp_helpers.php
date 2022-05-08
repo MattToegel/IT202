bc037dd77dede78eef9c37b077f8fabdd7617860
MattToegel <matthew.toegel@njit.edu>
2022-05-08 16:35:40 -0400

    added missing . append for line 175 of updating comp data

--- lib/comp_helpers.php
+++ lib/comp_helpers.php
@@ -172,7 +172,7 @@ function calc_winners()
     //closing calced comps
     if (count($calced_comps) > 0) {
         $query = "UPDATE RM_Competitions set did_calc = 1, did_payout = 1 WHERE id in ";
-        $query = "(" . str_repeat("?,", count($calced_comps) - 1) . "?)";
+        $query .= "(" . str_repeat("?,", count($calced_comps) - 1) . "?)";
         error_log("Close query: $query");
         $stmt = $db->prepare($query);
         try {
