29ae60124ee61c34ddfb39335f42a0d58ce37381
MattToegel <matthew.toegel@njit.edu>
2023-04-04 11:46:13 -0400

    merged roles lesson

--- public_html/Project/login.php
+++ public_html/Project/login.php
@@ -3,8 +3,8 @@ require_once(__DIR__ . "/../../partials/nav.php");
 ?>
 <form onsubmit="return validate(this)" method="POST">
     <div>
-        <label for="email">Email</label>
-        <input type="email" name="email" required />
+        <label for="email">Email/Username</label>
+        <input type="text" name="email" required />
     </div>
     <div>
         <label for="pw">Password</label>
@@ -40,9 +40,24 @@ if (isset($_POST["email"]) && isset($_POST["password"])) {
         flash("Please enter a valid email <br>");
         $hasError = true;
     }*/
-    if (!is_valid_email($email)) {
-        flash("Please enter a valid email <br>");
-        $hasError = true;
+    if (str_contains($email, "@")) {
+        //sanitize
+        //$email = filter_var($email, FILTER_SANITIZE_EMAIL);
+        $email = sanitize_email($email);
+        //validate
+        /*if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            flash("Invalid email address");
+            $hasError = true;
+        }*/
+        if (!is_valid_email($email)) {
+            flash("Invalid email address");
+            $hasError = true;
+        }
+    } else {
+        if (!is_valid_username($email)) {
+            flash("Invalid username");
+            $hasError = true;
+        }
     }
     if (empty($password)) {
         flash("Password must be provided <br>");
@@ -55,7 +70,7 @@ if (isset($_POST["email"]) && isset($_POST["password"])) {
     if (!$hasError) {
         //TODO 4
         $db = getDB();
-        $stmt = $db->prepare("SELECT id, email, username, password from Users where email = :email");
+        $stmt = $db->prepare("SELECT id, email, username, password from Users where email = :email or username = :email");
         try {
             $r = $stmt->execute([":email" => $email]);
             if ($r) {
