c50ee3e7f77ceb558b4443350cb4131c1b940f8b
MattToegel <matthew.toegel@njit.edu>
2024-09-30 20:08:53 -0400

    fixed login checkpoint

--- public_html/project/login.php
+++ public_html/project/login.php
@@ -59,7 +59,7 @@ if (isset($_POST["email"]) && isset($_POST["password"])) {
                     $hash = $user["password"];
                     unset($user["password"]);
                     if (password_verify($password, $hash)) {
-                        echo "Welcome $email";
+                        //echo "Welcome $email";
                         $_SESSION["user"] = $user;
                         die(header("Location: home.php"));
                     } else {
