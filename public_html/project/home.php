723a8bf2b1764732814dfad5de32c14a2b34314a
Matt Toegel <matthew.toegel@njit.edu>
2024-10-10 12:49:45 -0400

    another one

--- public_html/project/home.php
+++ public_html/project/home.php
@@ -4,8 +4,8 @@ require(__DIR__."/../../partials/nav.php");
 <h1>Home</h1>
 <?php
 error_log("SESSION DATA: " . var_export($_SESSION, true));
-if(isset($_SESSION["user"]) && isset($_SESSION["user"]["email"])){
- echo "Welcome, " . $_SESSION["user"]["email"]; 
+if(is_logged_in())){
+ echo "Welcome, " . get_user_email(); 
 }
 else{
   echo "You're not logged in";
