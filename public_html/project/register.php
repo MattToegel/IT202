7cb7912b934218880491b3267638384ac3d226f5
Matt Toegel <matthew.toegel@njit.edu>
2024-10-10 12:48:32 -0400

    Fixed missing login/reg stuff

--- public_html/project/register.php
+++ public_html/project/register.php
@@ -42,12 +42,17 @@ if (isset($_POST["email"]) && isset($_POST["password"]) && isset($_POST["confirm
         $hasError = true;
     }
     //sanitize
-    $email = filter_var($email, FILTER_SANITIZE_EMAIL);
+    //$email = filter_var($email, FILTER_SANITIZE_EMAIL);
+    $email = sanitize_email($email);
     //validate
-    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    if(!is_valid_email($email)){
+        echo "Invalid email address";
+
+    }
+    /*if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
         echo "Invalid email address";
         $hasError = true;
-    }
+    }*/
     if (empty($password)) {
         echo "password must not be empty";
         $hasError = true;
