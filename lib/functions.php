c2c6ba441748210f9f25254afa058f2436e75c08
MattToegel <matthew.toegel@njit.edu>
2024-10-17 20:50:19 -0400

    wip

--- lib/functions.php
+++ lib/functions.php
@@ -4,6 +4,9 @@ require_once(__DIR__ . "/db.php");
 //This is going to be a helper for redirecting to our base project path since it's nested in another folder
 //This MUST match the folder name exactly
 $BASE_PATH = '/project';
+//we moved the flash require higher so following functions have access to it
+//TODO 4: Flash Message Helpers
+require(__DIR__ . "/flash_messages.php");
 
 //require safer_echo.php
 require(__DIR__ . "/safer_echo.php");
@@ -12,5 +15,10 @@ require(__DIR__ . "/sanitizers.php");
 
 //TODO 3: User helpers
 require(__DIR__ . "/user_helpers.php");
-//TODO 4: Flash Message Helpers
+
+
+//duplicate email/username
+require(__DIR__ . "/duplicate_user_details.php");
+//reset session
+require(__DIR__ . "/reset_session.php");
 ?>
