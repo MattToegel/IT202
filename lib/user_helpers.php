684227a96f48736e8d8c8bd4897a1af120fa9f84
Matt Toegel <matthew.toegel@njit.edu>
2024-11-10 16:45:31 -0500

    Update user_helpers.php

--- lib/user_helpers.php
+++ lib/user_helpers.php
@@ -9,7 +9,7 @@ function is_logged_in($redirect = false, $destination = "login.php")
     if ($redirect && !$isLoggedIn) {
         //if this triggers, the calling script won't receive a reply since die()/exit() terminates it
         flash("You must be logged in to view this page", "warning");
-        redirect($destination);
+        die(header("Location: $destination"));
     }
     return $isLoggedIn;
 }
